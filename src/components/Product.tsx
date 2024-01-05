81cf0c2a0b991aeb3041dc6de660ef2fcd733643
Akram A. Abdelbasir <ashrfakrm64@gmail.com>
2024-01-05 11:30:07 +0200

    update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Link from "next/link";
 import { ProductType, StateProps } from "../../type";
 import Image from "next/image";
-import { Heart } from "lucide-react";
+import { Heart,ShoppingBasket  } from "lucide-react";
 import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, addToFavorite } from "@/redux/proSlice";
@@ -22,11 +22,11 @@ const Product = ({ products, prefix }: Item) => {
   const dispatch = useDispatch();
   return (
     <div className=' max-h-[800px] flex-1 overflow-y-auto'>
-      <div className="flex flex-wrap gap-6 mt-5 mx-auto">
+      <div className="flex flex-wrap gap-6 justify-center mt-5 mx-auto">
         {products?.map((item) => (
           <div
             key={`${item.id}_${item.title}`}
-            className="w-[45%] max-w-[250px] lg:w-[31%] rounded-[.5rem] p-3 relative bg-white group border-[1px] border-zinc-200 hover:border-zinc-400 duration-300 hover:shadow-xl overflow-hidden"
+            className="max-w-[250px] lg:w-[31%] rounded-[.5rem] p-3 relative bg-white group border-[1px] border-zinc-200 hover:border-zinc-400 duration-300 hover:shadow-xl overflow-hidden"
           >
             <Link href={{ pathname: `/id_${item?.id}`, query: { id: item?.id, prefix: item?.category } }}>
               <img
@@ -37,6 +37,7 @@ const Product = ({ products, prefix }: Item) => {
                 className="m-auto object-contain lg:object-contain group-hover:scale-105 duration-300"
               />
             </Link>
+            {/* <div className='flex gap-2'> */}
             <Heart
               fill={isFavorite(item.id) ? "red" : "black"}
               onClick={() => {
@@ -49,6 +50,16 @@ const Product = ({ products, prefix }: Item) => {
               }}
               className="absolute top-4 right-4 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200"
             />
+            <ShoppingBasket
+                  onClick={() => {
+                    dispatch(addToCart(item)),
+                      toast.success(`${item?.title} is added to Cart!`);
+                  }}
+                  className="absolute sm:hidden top-4 right-10 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200"
+                
+                  
+                />
+                {/* </div> */}
             <div className="hidden sm:block p-4 bg-zinc-100 group-hover:bg-zinc-50 group-hover:shadow-xl duration-300">
               <p className="whitespace-nowrap text-ellipsis overflow-hidden group-hover:text-designColor duration-300">
                 {item?.title}
