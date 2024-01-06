ea99bf1f081a3e4f99fa5c8d8c60708838aed755
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-01-06 19:27:10 +0000

    create login page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -461,7 +461,7 @@ const page = async () => {
   return (
     <Container>
       <div className="border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between">
-        <h2 className="text-2xl font-bold">About</h2>
+        <h2 className="text-2xl font-bold">Admin</h2>
       </div>
     </Container>
   );
--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,31 +1,31 @@
-
+import Link from 'next/link';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 
 const LoginComponent = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState(''); // Combined message state
+  const [route, setRoute] = useState('');
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('');
-  // const router = useRouter();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setIsSubmitDisabled(!e.target.value || !password);
+    setMessage(''); // Clear previous messages when input changes
   };
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
     setIsSubmitDisabled(!email || !e.target.value);
+    setMessage(''); // Clear previous messages when input changes
   };
 
   const handleSubmit = () => {
     if (email === 'ibrahem' && password === 'a') {
-      // Navigate to admin route
-      // router.push('/admin');
+      setRoute('/admin');
+      setMessage('You are authorized to login.'); // Set authorized message
     } else {
-      // Display forbidden message
-      setErrorMessage('Forbidden: Incorrect username or password.');
+      setMessage('Forbidden: Incorrect username or password.'); // Set error message
     }
   };
 
@@ -39,7 +39,7 @@ const LoginComponent = () => {
               <h1 className="text-2xl font-semibold">Login Form with Floating Labels</h1>
             </div>
             <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div className={`${route === "/admin" ? 'hidden' : 'block'} py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7`}>    
                 <div className="relative">
                   <input
                     id="email"
@@ -52,7 +52,7 @@ const LoginComponent = () => {
                   />
                   <label
                     htmlFor="email"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    className={`absolute left-0 -top-3.5 text-gray-600 text-sm ${email ? '-top-3.5' : 'peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2'} transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
                   >
                     Email Address
                   </label>
@@ -69,22 +69,29 @@ const LoginComponent = () => {
                   />
                   <label
                     htmlFor="password"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    className={`absolute left-0 -top-3.5 text-gray-600 text-sm ${password ? '-top-3.5' : 'peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2'} transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
                   >
                     Password
                   </label>
                 </div>
-                <div className="relative">
                   <button
                     onClick={handleSubmit}
-                    className={`bg-blue-500 text-white rounded-md px-2 py-1 ${isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    className={`bg-blue-500 '} text-white rounded-md px-2 py-1 ${isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
                     disabled={isSubmitDisabled}
                   >
                     Submit
                   </button>
-                  {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
-                </div>
               </div>
+                <div className="relative mt-5">
+                  {!route && message && <p className={`${message.includes('authorized') ? 'text-green-500' : 'text-red-500'}`}>{message}</p>}
+                  {route === "/admin" && (
+                    <div className='text-sm mt-2'>
+                      You are authorized now, click
+                      <Link className='underline hover:text-blue-600 mx-1' href={route}>Admin page</Link>
+                      to login
+                    </div>
+                  )}
+                </div>
             </div>
           </div>
         </div>
