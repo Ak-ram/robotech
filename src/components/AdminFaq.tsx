28142a2b7ce14a4fbe99e066b78004133f73a77d
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-01-07 08:14:28 +0000

    update

--- src/components/AdminFaq.tsx
+++ src/components/AdminFaq.tsx
@@ -22,121 +22,121 @@ const AdminFaq = () => {
 
     fetchData();
   }, []);
-  const handleQuestionChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewQuestion(event.target.value);
-  };
+  // const handleQuestionChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setNewQuestion(event.target.value);
+  // };
 
-  const handleAnswerChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewAnswer(event.target.value);
-  };
+  // const handleAnswerChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   setNewAnswer(event.target.value);
+  // };
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    setSelectedFile(file);
-  };
+  // const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const file = event.target.files?.[0];
+  //   setSelectedFile(file);
+  // };
 
-  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  // const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
 
-    // Add a new object to the JSON array
-    const updatedArray = [...jsonArray, { question: newQuestion, answer: newAnswer }];
+  //   // Add a new object to the JSON array
+  //   const updatedArray = [...jsonArray, { question: newQuestion, answer: newAnswer }];
 
-    const owner = 'Akram-44';
-    const repo = 'api';
-    const path = 'robotech/pages/faq.json';
-    const token = process.env.REACT_APP_GITHUB_TOKEN;
+  //   const owner = 'Akram-44';
+  //   const repo = 'api';
+  //   const path = 'robotech/pages/faq.json';
+  //   const token = process.env.REACT_APP_GITHUB_TOKEN;
 
-    const octokit = new Octokit({ auth: token });
+  //   const octokit = new Octokit({ auth: token });
 
-    try {
-      const response = await octokit.repos.getContent({
-        owner,
-        repo,
-        path,
-      });
+  //   try {
+  //     const response = await octokit.repos.getContent({
+  //       owner,
+  //       repo,
+  //       path,
+  //     });
 
-      if (!response || !response.data || !response.data.sha) {
-        setError('Invalid response or missing SHA.');
-        return;
-      }
+  //     if (!response || !response.data || !response.data.sha) {
+  //       setError('Invalid response or missing SHA.');
+  //       return;
+  //     }
 
-      const sha = response.data.sha;
+  //     const sha = response.data.sha;
 
-      // Update the file on GitHub with the modified content
-      await octokit.repos.createOrUpdateFileContents({
-        owner,
-        repo,
-        path,
-        message: 'Update JSON file',
-        content: Buffer.from(JSON.stringify(updatedArray, null, 2)).toString('base64'),
-        sha,
-      });
+  //     // Update the file on GitHub with the modified content
+  //     await octokit.repos.createOrUpdateFileContents({
+  //       owner,
+  //       repo,
+  //       path,
+  //       message: 'Update JSON file',
+  //       content: Buffer.from(JSON.stringify(updatedArray, null, 2)).toString('base64'),
+  //       sha,
+  //     });
 
-      // Update the local state after the file is successfully updated on GitHub
-      setJsonArray(updatedArray);
+  //     // Update the local state after the file is successfully updated on GitHub
+  //     setJsonArray(updatedArray);
 
-      console.log('JSON file updated successfully!');
-      setError(null);
-    } catch (error) {
-      console.error('Error updating JSON file:', error.message);
+  //     console.log('JSON file updated successfully!');
+  //     setError(null);
+  //   } catch (error) {
+  //     console.error('Error updating JSON file:', error.message);
 
-      if (error.status === 401) {
-        setError('Bad credentials or insufficient permissions.');
-      } else {
-        setError(`Error updating JSON file. Check console for details.`);
-      }
-    }
-  };
+  //     if (error.status === 401) {
+  //       setError('Bad credentials or insufficient permissions.');
+  //     } else {
+  //       setError(`Error updating JSON file. Check console for details.`);
+  //     }
+  //   }
+  // };
 
-  const handleUploadButtonClick = async () => {
-    if (selectedFile) {
-      const reader = new FileReader();
-      reader.onload = async (e) => {
-        const content = e.target?.result as string;
-        const parsedData = JSON.parse(content);
-        setJsonArray(parsedData);
+  // const handleUploadButtonClick = async () => {
+  //   if (selectedFile) {
+  //     const reader = new FileReader();
+  //     reader.onload = async (e) => {
+  //       const content = e.target?.result as string;
+  //       const parsedData = JSON.parse(content);
+  //       setJsonArray(parsedData);
 
-        const owner = 'Akram-44';
-        const repo = 'api';
-        const path = 'robotech/pages/faq.json';
-        const token = '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';
+  //       const owner = 'Akram-44';
+  //       const repo = 'api';
+  //       const path = 'robotech/pages/faq.json';
+  //       const token = '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';
 
-        const octokit = new Octokit({ auth: token });
+  //       const octokit = new Octokit({ auth: token });
 
-        try {
-          const response = await octokit.repos.getContent({
-            owner,
-            repo,
-            path,
-          });
-          if (!response || !response.data || !response.data.sha) {
-            setError('Invalid response or missing SHA.');
-            return;
-          }
+  //       try {
+  //         const response = await octokit.repos.getContent({
+  //           owner,
+  //           repo,
+  //           path,
+  //         });
+  //         if (!response || !response.data || !response.data.sha) {
+  //           setError('Invalid response or missing SHA.');
+  //           return;
+  //         }
 
-          const sha = response.data.sha;
+  //         const sha = response.data.sha;
 
-          // Update the file on GitHub with the content of the uploaded file
-          await octokit.repos.createOrUpdateFileContents({
-            owner,
-            repo,
-            path,
-            message: 'Update JSON file',
-            content: Buffer.from(JSON.stringify(parsedData, null, 2)).toString('base64'),
-            sha,
-          });
+  //         // Update the file on GitHub with the content of the uploaded file
+  //         await octokit.repos.createOrUpdateFileContents({
+  //           owner,
+  //           repo,
+  //           path,
+  //           message: 'Update JSON file',
+  //           content: Buffer.from(JSON.stringify(parsedData, null, 2)).toString('base64'),
+  //           sha,
+  //         });
 
-          console.log('JSON file updated successfully!');
-          setError(null);
-        } catch (error) {
-          console.error('Error updating JSON file:', error.message);
-          setError(`Error updating JSON file. Check console for details.`);
-        }
-      };
+  //         console.log('JSON file updated successfully!');
+  //         setError(null);
+  //       } catch (error) {
+  //         console.error('Error updating JSON file:', error.message);
+  //         setError(`Error updating JSON file. Check console for details.`);
+  //       }
+  //     };
 
-      reader.readAsText(selectedFile);
-    }
-  };
+  //     reader.readAsText(selectedFile);
+  //   }
+  // };
 
   return (
     <div className={` lg:p-3 w-full z-10 bottom-0 left-0 lg:relative overflow-hidden mt-5 `}>
@@ -162,13 +162,14 @@ const AdminFaq = () => {
 
         {error && <p className="text-red-500">{error}</p>}
 
-        <form onSubmit={handleFormSubmit} className="mt-4">
+        {/* <form onSubmit={handleFormSubmit} className="mt-4"> */}
+        {/* <form className="mt-4">
           <label className="block mb-2">
             Question:
             <input
               type="text"
               value={newQuestion}
-              onChange={handleQuestionChange}
+              // onChange={handleQuestionChange}
               className="border rounded px-4 py-2 w-full sm:w-64"
             />
           </label>
@@ -177,33 +178,33 @@ const AdminFaq = () => {
             <input
               type="text"
               value={newAnswer}
-              onChange={handleAnswerChange}
+              // onChange={handleAnswerChange}
               className="border rounded px-4 py-2 w-full sm:w-64"
             />
           </label>
           <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
             Add QA Pair
           </button>
-        </form>
+        </form> */}
 
-        <div className="mt-4">
+        {/* <div className="mt-4">
           <label className="block mb-2">
             Upload JSON File:
             <input
               type="file"
               accept=".json"
-              onChange={handleFileChange}
+              // onChange={handleFileChange}
               className="mt-2"
             />
           </label>
           <button
             type="button"
             className="bg-green-500 text-white px-4 py-2 rounded"
-            onClick={handleUploadButtonClick}
+            // onClick={handleUploadButtonClick}
           >
             Upload JSON File
           </button>
-        </div>
+        </div> */}
     </div>
   );
 };
