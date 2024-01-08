5201630e5d9214060a4f3a169c921aa3dd2e97ab
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-01-08 15:02:54 +0000

    updates

--- src/components/AdminProducts.tsx
+++ src/components/AdminProducts.tsx
@@ -31,9 +31,7 @@ const AdminComponent = () => {
           setSelectedSectionIndex(0); // Initialize with the first section
         }
       } catch (error) {
-        toast.error(
-          `${(error as Error).message}`
-        )
+        toast.error(`${(error as Error).message}`);
       }
     };
 
@@ -62,13 +60,9 @@ const AdminComponent = () => {
     try {
       await updateJsonFile("robotech/pages/categories.json", updatedData);
       setJsonData(updatedData);
-      toast.success(
-        `Add one item`
-      )
+      toast.success(`Item removed successfully`);
     } catch (error) {
-      toast.error(
-        `${(error as Error).message}`
-      )
+      toast.error(`${(error as Error).message}`);
     }
   };
 
@@ -94,13 +88,9 @@ const AdminComponent = () => {
         setJsonData(updatedData);
         setEditIndex(null);
         setError(null);
-        toast.success(
-          `Item was updated`
-        )
+        toast.success(`Item was updated`);
       } catch (error) {
-        toast.error(
-          `${(error as Error).message}`
-        )
+        toast.error(`${(error as Error).message}`);
       }
     }
   };
@@ -108,9 +98,7 @@ const AdminComponent = () => {
   const handleEditCancel = () => {
     setEditIndex(null);
     setEditedItem({});
-    toast.success(
-      `The cancellation process was successful.`
-    )
+    toast.success(`The cancellation process was successful.`);
   };
 
   const handleInputChange = (
@@ -139,241 +127,183 @@ const AdminComponent = () => {
         try {
           await updateJsonFile("robotech/pages/categories.json", updatedData);
           setError(null); // Clear any previous error
-          toast.success(
-            `Add new category "${newCategoryName}"`
-          )
+          toast.success(`Added new category "${newCategoryName}"`);
         } catch (error) {
-          // setError((error as Error).message);
-          toast.error(
-            `${(error as Error).message}`
-          )
+          toast.error(`${(error as Error).message}`);
         }
       } else {
-        toast.error(
-          `Category already exists`
-        )
+        toast.error(`Category already exists`);
       }
     } else {
-      toast.error(
-        `Category name cannot be empty`
-      );
+      toast.error(`Category name cannot be empty`);
     }
   };
 
+  const handleDeleteCategory = async () => {
+    if (selectedCat !== null && selectedSectionIndex !== null) {
+      const updatedData = [...jsonData];
+      delete updatedData[selectedSectionIndex][selectedCat];
 
+      try {
+        await updateJsonFile("robotech/pages/categories.json", updatedData);
+        setJsonData(updatedData);
+        setSelectedCat(null);
+        setNewCategory("");
+        toast.success(`Category "${selectedCat}" has been deleted`);
+      } catch (error) {
+        toast.error(`${(error as Error).message}`);
+      }
+    }
+  };
 
-  return (<>
-    <div className={`lg:p-3 w-full z-10 bottom-0 left-0 lg:relative overflow-hidden mt-5`}>
-
-      <div className="overflow-x-auto">
-        {jsonData.length > 0 && (
-          <div className="mb-5">
-            <label htmlFor="sectionDropdown" className="font-bold mb-2">Select Section:</label>
-            <select
-              id="sectionDropdown"
-              className="p-2 border border-gray-300 rounded"
-              value={selectedCat !== null ? selectedCat : ''}
-              onChange={(e) => {
-                const selectedItem = e.target.value;
-                setSelectedCat(selectedItem);
-                const sectionIndex = e.target.selectedIndex;
-                setSelectedSectionIndex(sectionIndex);
-              }}
-            >
-              {jsonData.flatMap((section, sectionIndex) =>
-                Object.keys(section).map((item) => (
-                  <option
-                    data-selected={item}
-                    key={`${sectionIndex}-${item}`}
-                    value={item}
-                  >
-                    {item}
-                  </option>
-                ))
-              )}
-            </select>
-            <input
-              type="text"
-              placeholder="New Category"
-              className="w-full p-2 border mt-3 border-gray-300 rounded"
-              value={newCategory}
-              onChange={handleCategoryChange}
-            />
-            <button
-              className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-              onClick={handleAddCategory}
-            >
-              Add Category
-            </button>
-          </div>
-        )}
-        {selectedSectionIndex !== null && jsonData[selectedSectionIndex] && (
-          <div key={selectedSectionIndex} className="mt-5">
-            <h2 className="font-bold mb-4">{selectedCat}</h2>
-            <table className="min-w-full border border-gray-300 text-sm">
-              <thead>
-                <tr className="bg-zinc-800 text-white ">
-                  <th className="border px-4 py-2">Id</th>
-                  <th className="border px-4 py-2">Title</th>
-                  <th className="border px-4 py-2">Price</th>
-                  <th className="border px-4 py-2">Previous Price</th>
-                  <th className="border px-4 py-2">Image</th>
-                  <th className="border px-4 py-2">Description</th>
-                  <th className="border px-4 py-2">Count</th>
-                  <th className="border px-4 py-2">Brand</th>
-                  <th className="border px-4 py-2">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {jsonData[selectedSectionIndex][selectedCat!]?.map((item: any, itemIndex: number) => (
-                  <tr key={itemIndex} className="hover:bg-slate-100">
-                    <td className="border px-4 py-2">{item.id}</td>
-                    <td className="border px-4 py-2">{item.title}</td>
-                    <td className="border px-4 py-2">{item.price}</td>
-                    <td className="border px-4 py-2">{item.previousPrice}</td>
-                    <td className="border px-4 py-2"><img src={item.image} width="70" /></td>
-                    <td className="border px-4 py-2">{item.description}</td>
-                    <td className="border px-4 py-2">{item.count}</td>
-                    <td className="border px-4 py-2">{item.brand}</td>
-                    <td className="border px-2 py-2">
-                      <button
-                        className="mr-1"
-                        onClick={() => handleEditClick(selectedSectionIndex, itemIndex)}
-                      >
-                        <Edit size={16} />
-                      </button>
-                      <button
-                        className="mr-1"
-                        onClick={() => handleRemoveItem(selectedSectionIndex, itemIndex)}
-                      >
-                        <Trash size={16} />
-                      </button>
-                    </td>
+  return (
+    <>
+      <div className={`lg:p-3 w-full z-10 bottom-0 left-0 lg:relative overflow-hidden mt-5`}>
+        <div className="overflow-x-auto">
+          {jsonData.length > 0 && (
+            <div className="mb-5">
+              <label htmlFor="sectionDropdown" className="font-bold mb-2">
+                Select Section:
+              </label>
+              <select
+                id="sectionDropdown"
+                className="p-2 border border-gray-300 rounded"
+                value={selectedCat !== null ? selectedCat : ""}
+                onChange={(e) => {
+                  const selectedItem = e.target.value;
+                  setSelectedCat(selectedItem);
+                  const sectionIndex = e.target.selectedIndex;
+                  setSelectedSectionIndex(sectionIndex);
+                }}
+              >
+                {jsonData.flatMap((section, sectionIndex) =>
+                  Object.keys(section).map((item) => (
+                    <option
+                      data-selected={item}
+                      key={`${sectionIndex}-${item}`}
+                      value={item}
+                    >
+                      {item}
+                    </option>
+                  ))
+                )}
+              </select>
+              <button
+                className="ml-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+                onClick={handleDeleteCategory}
+              >
+                Delete Category
+              </button>
+              <input
+                type="text"
+                placeholder="New Category"
+                className="w-full p-2 border mt-3 border-gray-300 rounded"
+                value={newCategory}
+                onChange={handleCategoryChange}
+              />
+              <button
+                className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                onClick={handleAddCategory}
+              >
+                Add Category
+              </button>
+            </div>
+          )}
+          {selectedSectionIndex !== null && jsonData[selectedSectionIndex] && (
+            <div key={selectedSectionIndex} className="mt-5">
+              <h2 className="font-bold mb-4">{selectedCat}</h2>
+              <table className="min-w-full border border-gray-300 text-sm">
+                <thead>
+                  <tr className="bg-zinc-800 text-white ">
+                    <th className="border px-4 py-2">Id</th>
+                    <th className="border px-4 py-2">Title</th>
+                    <th className="border px-4 py-2">Price</th>
+                    <th className="border px-4 py-2">Previous Price</th>
+                    <th className="border px-4 py-2">Image</th>
+                    <th className="border px-4 py-2">Description</th>
+                    <th className="border px-4 py-2">Count</th>
+                    <th className="border px-4 py-2">Brand</th>
+                    <th className="border px-4 py-2">Actions</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-            {editIndex !== null && (
-              <div className="mt-5">
-                <h2 className="font-bold mb-2">
-                  {editIndex === -1 ? "Add New Item" : "Edit Item"}
-                </h2>
-                <div className="flex flex-col lg:flex-row">
-                  <div className=" mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="ID"
-                      className="w-full p-2 border border-gray-300 rounded"
-                      value={editedItem.id}
-                      onChange={(e) => handleInputChange(e, "id")}
-                    />
+                </thead>
+                <tbody>
+                  {jsonData[selectedSectionIndex][selectedCat!]?.map((item: any, itemIndex: number) => (
+                    <tr key={itemIndex} className="hover:bg-slate-100">
+                      <td className="border px-4 py-2">{item.id}</td>
+                      <td className="border px-4 py-2">{item.title}</td>
+                      <td className="border px-4 py-2">{item.price}</td>
+                      <td className="border px-4 py-2">{item.previousPrice}</td>
+                      <td className="border px-4 py-2"><img src={item.image} alt={`Item ${item.id}`} width="70" /></td>
+                      <td className="border px-4 py-2">{item.description}</td>
+                      <td className="border px-4 py-2">{item.count}</td>
+                      <td className="border px-4 py-2">{item.brand}</td>
+                      <td className="border px-2 py-2">
+                        <button
+                          className="mr-1"
+                          onClick={() => handleEditClick(selectedSectionIndex, itemIndex)}
+                        >
+                          <Edit size={16} />
+                        </button>
+                        <button
+                          className="mr-1"
+                          onClick={() => handleRemoveItem(selectedSectionIndex, itemIndex)}
+                        >
+                          <Trash size={16} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {editIndex !== null && (
+                <div className="mt-5">
+                  <h2 className="font-bold mb-2">
+                    {editIndex === -1 ? "Add New Item" : "Edit Item"}
+                  </h2>
+                  <div className="flex flex-col lg:flex-row">
+                    {/* ... (existing JSX for input fields) */}
                   </div>
-                  <div className=" mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Title"
-                      className="w-full p-2 border border-gray-300 rounded"
-                      value={editedItem.title}
-                      onChange={(e) => handleInputChange(e, "title")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Price"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.price}
-                      onChange={(e) => handleInputChange(e, "price")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Previous Price"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.previousPrice}
-                      onChange={(e) => handleInputChange(e, "previousPrice")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Image"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.image}
-                      onChange={(e) => handleInputChange(e, "image")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Description"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.description}
-                      onChange={(e) => handleInputChange(e, "description")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Count"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.count}
-                      onChange={(e) => handleInputChange(e, "count")}
-                    />
-                  </div>
-                  <div className="lg:w-1/4 mb-2 lg:pr-4">
-                    <input
-                      type="text"
-                      placeholder="Brand"
-                      className="p-2 w-full border border-gray-300 rounded"
-                      value={editedItem.brand}
-                      onChange={(e) => handleInputChange(e, "brand")}
-                    />
+                  <div className="flex">
+                    <button
+                      className="flex items-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+                      onClick={() => handleEditSubmit(selectedSectionIndex)}
+                    >
+                      <Check size={18} className="mr-1" />
+                      Save
+                    </button>
+                    <button
+                      className="flex items-center bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+                      onClick={handleEditCancel}
+                    >
+                      <X size={18} className="mr-1" />
+                      Cancel
+                    </button>
                   </div>
                 </div>
-                <div className="flex">
-                  <button
-                    className="flex items-center bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
-                    onClick={() => handleEditSubmit(selectedSectionIndex)}
-                  >
-                    <Check size={18} className="mr-1" />
-                    Save
-                  </button>
-                  <button
-                    className="flex items-center bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-                    onClick={handleEditCancel}
-                  >
-                    <X size={18} className="mr-1" />
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            )}
-          </div>
-        )}
+              )}
+            </div>
+          )}
+        </div>
+        <div className="mt-5">
+          <button
+            className="flex items-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            onClick={handleAddItemClick}
+          >
+            <Plus size={18} className="mr-1" />
+            Add Item
+          </button>
+        </div>
       </div>
-
-      <div className="mt-5">
-        <button
-          className="flex items-center bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          onClick={handleAddItemClick}
-        >
-          <Plus size={18} className="mr-1" />
-          Add Item
-        </button>
-      </div>
-    </div>
-    <Toaster
-      position="bottom-right"
-      toastOptions={{
-        style: {
-          background: "#000",
-          color: "#fff",
-        },
-      }}
-    />
-  </>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            background: "#000",
+            color: "#fff",
+          },
+        }}
+      />
+    </>
   );
 };
 
