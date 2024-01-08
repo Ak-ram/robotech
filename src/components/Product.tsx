aa831aaed2d88f2eebaba0559132932532be1b94
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-01-08 17:21:14 +0000

    updates

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Link from "next/link";
 import { ProductType, StateProps } from "../../type";
 import Image from "next/image";
-import { Heart,ShoppingBasket  } from "lucide-react";
+import { Heart,ShoppingBasket, ShoppingCart  } from "lucide-react";
 import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, addToFavorite } from "@/redux/proSlice";
@@ -21,25 +21,25 @@ const Product = ({ products, prefix }: Item) => {
   };
   const dispatch = useDispatch();
   return (
-    <div className=' max-h-[800px] flex-1 overflow-y-auto'>
-      <div className="flex flex-wrap  gap-6  mt-5 mx-auto">
-        {products?.map((item) => (
-          <div
-            key={`${item.id}_${item.title}`}
-            className="w-[45%] max-w-[250px] lg:w-[31%] rounded-[.5rem] p-3 relative bg-white group border-[1px] border-zinc-200 hover:border-zinc-400 duration-300 hover:shadow-xl overflow-hidden"
-          >
-            <Link href={{ pathname: `/id_${item?.id}`, query: { id: item?.id, prefix: item?.category } }}>
+    <div className="max-h-[800px] flex-1 overflow-y-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5 mx-auto">
+      {products?.map((item) => (
+        <div
+          key={`${item.id}_${item.title}`}
+          className="relative bg-white group border-[1px] border-zinc-200 hover:border-zinc-400 duration-300 hover:shadow-xl overflow-hidden rounded-md"
+        >
+          <Link href={{ pathname: `/id_${item?.id}`, query: { id: item?.id, prefix: item?.category } }}>
+            <div className="aspect-w-2 aspect-h-3">
               <img
                 src={item?.image}
                 alt="Product image"
-                width={200}
-                height={300}
-                className="m-auto object-contain lg:object-contain group-hover:scale-105 duration-300"
+                className="object-cover object-center w-full h-full group-hover:scale-105 duration-300"
               />
-            </Link>
-            {/* <div className='flex gap-2'> */}
+            </div>
+          </Link>
+          <div className="absolute top-2 right-2 flex items-center space-x-2">
             <Heart
-              fill={isFavorite(item.id) ? "red" : "black"}
+              fill={isFavorite(item.id) ? 'red' : 'black'}
               onClick={() => {
                 dispatch(addToFavorite(item));
                 if (isFavorite(item?.id)) {
@@ -48,52 +48,48 @@ const Product = ({ products, prefix }: Item) => {
                   toast.success(`${item?.title} added to favorites!`);
                 }
               }}
-              className="absolute top-4 right-4 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200"
+              className="text-zinc-500 w-5 h-5 cursor-pointer duration-200 hover:text-black"
             />
-            <ShoppingBasket
-                  onClick={() => {
-                    dispatch(addToCart(item)),
-                      toast.success(`${item?.title} is added to Cart!`);
-                  }}
-                  className="absolute sm:hidden top-4 right-10 text-zinc-500 w-5 h-5 hover:text-black cursor-pointer duration-200"
-                
-                  
-                />
-                {/* </div> */}
-            <div className="hidden sm:block p-4 bg-zinc-100 group-hover:bg-zinc-50 group-hover:shadow-xl duration-300">
-              <p className="whitespace-nowrap text-ellipsis overflow-hidden group-hover:text-designColor duration-300">
-                {item?.title}
-              </p>
-              <p className="font-semibold">
-                <FormattedPrice amount={item?.price} />
-              </p>
-              <div className="flex items-center justify-between text-sm mt-2">
-                <button
-                  onClick={() => {
-                    dispatch(addToCart(item)),
-                      toast.success(`${item?.title} is added to Cart!`);
-                  }}
-                  className="uppercase font-semibold hover:text-designColor duration-300"
-                >
-                  Add to cart
-                </button>
-              </div>
+            <ShoppingCart
+              onClick={() => {
+                dispatch(addToCart(item));
+                toast.success(`${item?.title} is added to Cart!`);
+              }}
+              className="text-zinc-500 w-5 h-5 cursor-pointer duration-200 hover:text-black"
+            />
+          </div>
+          <div className="p-4">
+            <p className="whitespace-nowrap text-ellipsis overflow-hidden group-hover:text-designColor duration-300 font-bold">
+              {item?.title}
+            </p>
+            <p className="text-designColor font-semibold">
+              <FormattedPrice amount={item?.price} />
+            </p>
+            <div className="flex items-center justify-between mt-2">
+              <button
+                onClick={() => {
+                  dispatch(addToCart(item));
+                  toast.success(`${item?.title} is added to Cart!`);
+                }}
+                className="uppercase font-semibold text-white bg-designColor py-2 px-4 rounded-full hover:bg-opacity-80 duration-300"
+              >
+                Add to Cart
+              </button>
             </div>
           </div>
-        ))}
-        <Toaster
-          position="bottom-right"
-          toastOptions={{
-            style: {
-              background: "#000",
-              color: "#fff",
-            },
-          }}
-        />
-      </div>
-
-
+        </div>
+      ))}
     </div>
+    <Toaster
+      position="bottom-right"
+      toastOptions={{
+        style: {
+          background: '#000',
+          color: '#fff',
+        },
+      }}
+    />
+  </div>
   );
 };
 
