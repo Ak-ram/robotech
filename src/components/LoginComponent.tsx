e79a8d076f61fe6d2d07448075fca82660895469
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2024-01-15 11:44:49 +0000

    updates

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -55,8 +55,8 @@ const LoginComponent = () => {
   return (
     <div className="bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
               <h1 className="text-2xl font-semibold">Login Form with Floating Labels</h1>
