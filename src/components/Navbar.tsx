83abbec9f6398d043eef8628767da888eda6174f
akram-444 <156472261+akram-444@users.noreply.github.com>
2024-01-22 20:34:39 +0000

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,12 +61,12 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="w-full py-2 border-b-[1px] border-b-zinc-500 bg-white text-zinc-600 sticky top-0 z-50 bg-white/80 backdrop-blur-2xl">
-      <div className="order-first flex-wrap max-w-screen-xl mx-auto h-full flex items-center px-4 xl:px-0">
+    <div className=" py-2 border-b-[1px] border-b-zinc-500 bg-white text-zinc-600 sticky top-0 z-50 bg-white/80 backdrop-blur-2xl">
+      <div className="w-[98%] mx-auto order-first flex-wrap max-w-screen-xl mx-auto h-full flex items-center px-4 xl:px-0">
         {/* Logo */}
         <Logo />
         {/* Navigation */}
-        <ul className="order-last text-xs sm:text-sm flex-1  md:order-none w-full md:w-fit flex py-2 items-center gap-5 text-sm uppercase font-semibold">
+        <ul className="order-last text-xs sm:text-sm flex-1  md:order-none md:justify-center w-full md:w-fit flex py-2 items-center gap-5 text-sm uppercase font-semibold">
           {navigation.map((item) => (
             <Link href={item?.href} key={item.title}>
               <li
@@ -82,44 +82,10 @@ const Navbar = () => {
             </Link>
           ))}
         </ul>
-        {/* <div className="order-last mt-2 md:mt-0 relative flex-1 md:mx-4 relative flex justify-center items-center">
-          <input
-            onInput={(e: ChangeEvent<HTMLInputElement>) =>
-              searching(e.target.value)
-            }
-            className="outline-none bg-slate-200 rounded-md px-4 py-2 md:py-3 w-full max-width-lg"
-            type="search"
-            placeholder="Search..."
-          />
-          <ul
-            className={`${isInput ? "block py-1 lg:py-3 " : "hidden p-0 "
-              } shadow-lg mx-0 top-11 w-full border shadow-md border-zinc-400 absolute bg-white mt-2 rounded-sm`}
-          >
-            {res.length > 0 ? (
-              res.map((item) => (
-                <li
-                  key={item.title}
-                  className={`${isInput ? "py-1 lg:py-2.5 border-b" : "p-0 border-0"} hover:bg-zinc-100 cursor-pointer hover:bg-slate-200 rounded-sm my-1`}
-                >
-                  <Link
-                    className='flex items-center font-bold justify-between px-3'
-                    href={{ pathname: `/id_${item?.id}`, query: { id: item?.id, prefix: item?.category } }}>
-                    <span className='flex-col flex'>
-                      <span className='text-sm mb-2 md:text-lg text-blue-400'>{item.title}</span>
-                      <span className='text-xs font-bold'><FormattedPrice amount={item?.price} /></span>
-                    </span>
-                    <img className='hidden md:inline-block rounded-md' src={item.image1} width={70} height={70} />
-                  </Link>
-                </li>
-              ))
-            ) : (
-              <li>No results found</li>
-            )}
-          </ul>
-        </div> */}
+
         {/* icons */}
         <div className="w-[70%] justify-end md:w-fit md:justify-start ml-auto flex items-center gap-x-5">
-        <span
+          <span
             className="hover:text-black cursor-pointer duration-200 relative group"
           >
             <Search className="md:w-6 md:h-6" />
@@ -147,18 +113,53 @@ const Navbar = () => {
             </span>
           </Link>
 
-<div className='border-l pl-3 hidden md:flex items-center text-xs gap-2 text-zinc-600'>
-  <PhoneCall className='text-designColor md:w-6 md:h-6' />
-  <div className='flex flex-col gap-1'>
-    <span><span className='font-bold  text-black'>Support </span>(+20) 11 0207 1544</span>
-    <span>Email: robotechspace8@gmail.com</span>
-  </div>
-</div>
+          <div className='border-l pl-3 hidden md:flex items-center text-xs gap-2 text-zinc-600'>
+            <PhoneCall className='text-designColor md:w-6 md:h-6' />
+            <div className='flex flex-col gap-1'>
+              <span><span className='font-bold  text-black'>Support </span>(+20) 11 0207 1544</span>
+              <span>Email: robotechspace8@gmail.com</span>
+            </div>
+          </div>
         </div>
 
       </div>
-    </div>
-   
+      <div className="w-[400px] order-last mt-2 md:mt-0 relative flex-1 md:mx-4 relative flex justify-center items-center">
+      <Search className="absolute top-7 left-2 md:w-5 md:h-5" />
+        <input
+          onInput={(e: ChangeEvent<HTMLInputElement>) =>
+            searching(e.target.value)
+          }
+          className="outline-none h-10 pl-10 border border-slate-100 rounded-md pr-4 py-2 md:py-3 mr-auto mt-4  max-width-lg"
+          type="search"
+          placeholder="Search..."
+        />
+        <ul
+          className={`${isInput ? "block py-1 lg:py-3 " : "hidden p-0 "
+            } shadow-lg mx-0 top-11 w-full border shadow-md border-zinc-400 absolute bg-white mt-2 rounded-sm`}
+        >
+          {res.length > 0 ? (
+            res.map((item) => (
+              <li
+                key={item.title}
+                className={`${isInput ? "py-1 lg:py-2.5 border-b" : "p-0 border-0"} hover:bg-zinc-100 cursor-pointer hover:bg-slate-200 rounded-sm my-1`}
+              >
+                <Link
+                  className='flex items-center font-bold justify-between px-3'
+                  href={{ pathname: `/id_${item?.id}`, query: { id: item?.id, prefix: item?.category } }}>
+                  <span className='flex-col flex'>
+                    <span className='text-sm mb-2 md:text-lg text-blue-400'>{item.title}</span>
+                    <span className='text-xs font-bold'><FormattedPrice amount={item?.price} /></span>
+                  </span>
+                  <img className='hidden md:inline-block rounded-md' src={item.image1} width={70} height={70} />
+                </Link>
+              </li>
+            ))
+          ) : (
+            <li>No results found</li>
+          )}
+        </ul>
+      </div>     </div>
+
   );
 };
 
