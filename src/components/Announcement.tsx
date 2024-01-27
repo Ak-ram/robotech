b1b391cf7bf50cc7fc8737e41b4ece43587b0456
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-27 09:57:16 +0000

    updates

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { getAnnouncementData } from "@/helpers/getAnnouncement";
+import { X } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -11,7 +12,7 @@ interface AnnouncementT {
 
 const Announcement = () => {
     const [data, setData] = useState<AnnouncementT[]>([]);
-
+    const [closed, setClosing] = useState<Boolean>(false)
     useEffect(() => {
         const fetchAnnouncement = async () => {
             try {
@@ -26,21 +27,22 @@ const Announcement = () => {
             fetchAnnouncement();
         }
     }, []);
+    let announcement = closed ? null : <div className="bg-gray-900 text-white py-3 px-5 flex">
+        <div className="container mx-auto flex items-center justify-between">
+            <p className="text-sm font-semibold">
+                {data.length > 0 && data[0].body}
+            </p>
+            {data.length > 0 && (
+                <Link href={data[0].link_url} className="font-bold text-sm underline hover:text-gray-200">
+                    {data[0].link_text}
 
-    return (
-        <div className="bg-gray-900 text-white py-3 px-4">
-            <div className="container mx-auto flex items-center justify-between">
-                <p className="text-sm font-semibold">
-                    {data.length > 0 && data[0].body}
-                </p>
-                {data.length > 0 && (
-                    <Link href={data[0].link_url} className="font-bold text-sm underline hover:text-gray-200">
-                        {data[0].link_text}
-
-                    </Link>
-                )}
-            </div>
+                </Link>
+            )}
         </div>
+        <span className="cursor-pointer" onClick={() => setClosing(true)}><X /></span>
+    </div>
+    return (
+        announcement
     );
 };
 
