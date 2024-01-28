961008bc5d25a59f584c06e96daf859b7e079e94
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-28 13:19:42 +0000

    updates

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const page = async () => {
   // const products = await getFaq();
   return (
-    <Container>
+    <Container className="py-10">
       <div className="border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between">
         <h2 className="font-bold text-2xl">FAQ</h2>
       </div>
