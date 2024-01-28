a4eb580312c4546134d154e2248de27d6ebabbc7
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-28 08:22:51 +0000

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 
 
   return (
-    <div className=" py-2 border-b-[1px] border-b-zinc-500 bg-white text-zinc-600 sticky top-0 z-50 bg-white/80 backdrop-blur-2xl">
+    <div className=" py-2 border-b-[1px] border-b-zinc-500 bg-zinc-900 text-white/80 sticky top-0 z-50  backdrop-blur-2xl">
       <div className="w-[98%] mx-auto order-first flex-wrap max-w-screen-xl mx-auto h-full flex items-center px-4 xl:px-0">
         {/* Logo */}
         <Logo />
@@ -51,7 +51,7 @@ const Navbar = () => {
           {navigation.map((item) => (
             <Link href={item?.href} key={item.title}>
               <li
-                className={`mt-2 hover:text-black cursor-pointer duration-200 relative overflow-hidden group ${item.href === pathname && "text-designColor"
+                className={`mt-2 hover:text-white cursor-pointer duration-200 relative overflow-hidden group ${item.href === pathname && "text-white"
                   }`}
               >
                 {item?.title}
@@ -67,10 +67,10 @@ const Navbar = () => {
         {/* icons */}
         <div className="w-[70%] justify-end md:w-fit md:justify-start ml-auto flex items-center gap-x-5">
 
-          <div className='border-l border-r px-3 hidden md:flex items-center text-xs gap-2 text-zinc-600'>
-            <PhoneCall className='text-designColor md:w-6 md:h-6' />
+          <div className='border-l border-r px-3 hidden md:flex items-center text-xs gap-2 text-white/80'>
+            <PhoneCall className='text-white/80 md:w-6 md:h-6' />
             <div className='flex flex-col gap-1'>
-              <span><span className='font-bold  text-black'>Support </span>(+20) 11 0207 1544</span>
+              <span><span className='font-bold  text-white'>Support </span>(+20) 11 0207 1544</span>
               <span>robotechspace8@gmail.com</span>
             </div>
           </div>
