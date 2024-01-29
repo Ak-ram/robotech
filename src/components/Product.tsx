4cb2e95982b9bc6799c4003a3a1c26689ab76b0d
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-29 21:05:45 +0000

    updates

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { ProductType, StateProps } from "../../type";
 import Image from "next/image";
-import { Heart, ShoppingBasket, ShoppingBasketIcon, ShoppingCart } from "lucide-react";
+import { ChevronLeft, ChevronRight, Heart, ShoppingBasket, ShoppingBasketIcon, ShoppingCart } from "lucide-react";
 import FormattedPrice from "./FormattedPrice";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, addToFavorite } from "@/redux/proSlice";
@@ -41,8 +41,67 @@ const Product = ({ products, prefix, categoryName }: Item) => {
   };
 
   return (
-    <div className="flex-1">
-      <div className="container max-w-4xl m-auto flex flex-wrap items-start justify-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mt-5 mx-auto">
+    <div className="flex-1 pt-5 flex items-center justify-end">
+      {/* <nav aria-label="Page navigation example">
+          <ul className="flex items-center -space-x-px h-10 text-base">
+            <button
+              className={`${perPage?.start === 0 ? 'cursor-not-allowed' : ''}`}
+              onClick={handlePrev}
+              disabled={perPage.start === 0}
+            >
+              <div className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                <span className="">Previous</span>
+                <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                </svg>
+              </div>
+            </button>
+
+            <button
+              className={`${perPage?.end >= products?.length ? 'cursor-not-allowed' : ''}`}
+              onClick={handleNext}
+              disabled={perPage?.end >= products?.length}>
+              <div className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                <span className="">Next</span>
+                <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                </svg>
+              </div>
+            </button>
+          </ul>
+        </nav> */}
+      <nav aria-label="Page navigation example">
+        <ul className="flex items-center -space-x-px h-8 text-sm">
+          <li>
+            <button  
+            className={`${perPage?.start === 0 ? 'cursor-not-allowed' : ''} flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+              onClick={handlePrev}
+              disabled={perPage.start === 0}
+               >
+              <span className="sr-only">Previous</span>
+              <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+              </svg>
+            </button>
+          </li>
+          <li>
+            <button 
+             className={`${perPage?.end >= products?.length ? 'cursor-not-allowed' : ''} flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+              onClick={handleNext}
+              disabled={perPage?.end >= products?.length} 
+              >
+              <span className="sr-only">Next</span>
+              <svg className="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+
+      <div className="container max-w-4xl m-auto flex flex-wrap items-start justify-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mt-2 mx-auto">
+
         {products ? products.slice(perPage.start, perPage.end)?.map((item) => (
 
           <div
@@ -169,22 +228,9 @@ const Product = ({ products, prefix, categoryName }: Item) => {
 
       <div className={`${products?.length > 9 ? 'flex' : 'hidden'}  justify-between mt-4`}>
 
-        <div className="flex w-full justify-between mt-4">
-          <button
-            className={`${perPage?.start === 0 ? 'cursor-not-allowed' : ''} text-sm bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded`}
-            onClick={handlePrev}
-            disabled={perPage.start === 0}
-          >
-            Prev
-          </button>
-          <button
-            className={`${perPage?.end >= products?.length ? 'cursor-not-allowed' : ''} text-sm bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded`}
-            onClick={handleNext}
-            disabled={perPage?.end >= products?.length}
-          >
-            Next
-          </button>
-        </div>
+
+
+
       </div>
       <Toaster
         position="bottom-right"
