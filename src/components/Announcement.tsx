44bf4966d5d756f23255b282e2d7ba6fa6439f6d
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-29 20:07:11 +0000

    updates

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { getAnnouncementData } from "@/helpers/getAnnouncement";
-import { X } from "lucide-react";
+import shortLogo from '@/assets/ShortLogo.png'
+import { ChevronRight, Feather, X } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface AnnouncementT {
     body: string;
@@ -27,20 +29,20 @@ const Announcement = () => {
             fetchAnnouncement();
         }
     }, []);
-    let announcement = closed ? null : <div className="items-center bg-blue-500 text-white py-3 px-5 flex">
+    let announcement = closed ? null : <div className="items-start sm:items-center  bg-blue-500 text-white py-3 px-5 flex">
         <div className="container w-[90%] mx-auto sm:gap-3 flex flex-col items-start sm:flex-row sm:items-center">
-            <span className="slide-tr -mr-8">🚀</span>
-            <p className="whitespace-nowrap overflow-auto w-[99%] flex-1 text-xs sm:text-sm font-semibold">
+            <p className="flex flex-nowrap gap-1 items-center overflow-auto w-[99%]  text-xs sm:text-sm font-semibold">
+            <Feather  width={25} height={25} className=""/>
                 {data.length > 0 && data[0].body}
             </p>
             {data.length > 0 && (
-                <Link href={data[0].link_url} className="font-bold text-xs sm:text-sm mt-2 sm:mt-0 underline hover:text-gray-200">
+                <Link href={data[0].link_url} className="flex items-end gap-1 flex-nowrap whitespace-nowrap font-bold text-xs sm:text-sm mt-2 sm:mt-0 underline hover:text-gray-200">
                     {data[0].link_text}
-
+                    <ChevronRight size={16}/>
                 </Link>
             )}
         </div>
-        <span className="hover:text-red-500 cursor-pointer ml-3" onClick={() => setClosing(true)}><X /></span>
+        <span className="hover:text-white text-white/80 cursor-pointer mt-2 sm:mt-0 ml-3" onClick={() => setClosing(true)}><X className="" size={20}/></span>
     </div>
     return (
         announcement
