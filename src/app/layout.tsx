5c16c6960489a46b669e2f1b1b8ed801b200ab41
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-01-31 13:17:21 +0000

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Alegreya_Sans_SC} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
@@ -9,7 +9,8 @@ import Layout from "@/components/Layout";
 import Announcement from "@/components/Announcement";
 import WhatsApp from "@/components/WhatsApp";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Alegreya_Sans_SC({weight:"700",subsets: ["latin"]});
+// Available subsets: `cyrillic`, `cyrillic-ext`, `greek`, `greek-ext`, `latin`, `latin-ext`, `vietnamese`
 
 export const metadata: Metadata = {
   title: "Robotech space",
--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -11,7 +11,7 @@ const Categories = ({ categories, setCategoryName }) => {
 
         <div className="flex flex-col">
 
-          <span className="hidden lg:block ml-3 mt-5 lg:mt-10 mb-2 block text-xs font-semibold text-gray-500">Product Mangement</span>
+          <span className="hidden lg:block ml-3 mt-5 lg:mt-10 mb-2 block text-base font-semibold text-gray-500">Product Mangement</span>
 
           <nav className="text-zinc-600 flex lg:block overflow-x-auto">
             {categories?.map((cat_title: string, i: number) => (
@@ -21,7 +21,7 @@ const Categories = ({ categories, setCategoryName }) => {
                   setCategoryName(cat_title);
                   setSelectedCategory(cat_title); // Set the selected category
                 }}
-                className={`lg:w-full whitespace-nowrap ${selectedCategory === cat_title ? 'bg-gray-100 text-black' : 'text-zinc-500 hover:bg-zinc-100 hover:text-zinc-600'} flex cursor-pointer items-center lg:border-l-designColor py-2 px-4 text-sm font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor focus:border-l-4`}>
+                className={`lg:w-full whitespace-nowrap ${selectedCategory === cat_title ? 'bg-gray-100 text-black' : 'text-zinc-500 hover:bg-zinc-100 hover:text-zinc-600'} flex cursor-pointer items-center lg:border-l-designColor py-2 px-4 font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor focus:border-l-4`}>
                 <svg className="mr-4 h-5 w-5 align-middle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" className=""></path>
                 </svg>
@@ -30,15 +30,15 @@ const Categories = ({ categories, setCategoryName }) => {
             ))}
           </nav>
 
-          <span className="hidden ml-3 lg:mt-10 mb-2 lg:block text-xs font-semibold text-gray-500">Pages</span>
+          <span className="hidden ml-3 lg:mt-10 mb-2 lg:block text-base font-semibold text-gray-500">Pages</span>
 
           <nav className="hidden justify-center lg:items-start text-zinc-600 flex lg:block overflow-x-auto">
             {navigation.map((item) => (
-              <Link href={item?.href} key={item._id} className={`hover:bg-zinc-100 flex cursor-pointer items-center border-l-designColor py-2 px-4 text-sm font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor-600 focus:border-l-4`}>
+              <Link href={item?.href} key={item._id} className={`hover:bg-zinc-100 flex cursor-pointer items-center border-l-designColor py-2 px-4 font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor-600 focus:border-l-4`}>
                 <svg className="mr-4 h-5 w-5 align-middle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                 </svg>
-                {item?.title}
+                {item?.title.slice(0,1).toUpperCase() + item?.title.slice(1)}
               </Link>
             ))}
 
