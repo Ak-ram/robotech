21ea2b0a21239a9258e839c3eb0b20472117a5eb
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-02-01 17:29:06 +0000

    updates

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react"; // Import useState from React
 const Categories = ({ categories, setCategoryName, openSidebar, setOpenSidebar, products }) => {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={`${openSidebar ? "translate-x-0" : "-translate-x-full md:translate-x-0"} h-fit transition  -left-7 top-5 absolute lg:relative lg:min-w-[350px] lg:w-[350px] p-3 z-10 border-t-0 bottom-0 left-0 overflow-hidden`}>
       <div className="flex h-full flex-grow flex-col p-3 overflow-y-auto bg-white shadow-md">
@@ -28,33 +29,36 @@ const Categories = ({ categories, setCategoryName, openSidebar, setOpenSidebar, 
               <>
                 <button
                   key={`${cat_title}_${i}`}
-                  onClick={() => {
+
+                  className={`text-ellipsis overflow-hidden lg:w-full whitespace-nowrap ${selectedCategory === cat_title ? 'bg-gray-100 text-black' : 'text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950'} font-semibold flex cursor-pointer items-center lg:border-l-designColor py-2 px-4 font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor focus:border-l-4  text-sm md:text-base`}>
+
+                  {
+                    selectedCategory === cat_title && isOpen ? <MinusSquare onClick={() => setIsOpen(false)} className="mr-3" size={18} /> : <PlusSquare onClick={() => setIsOpen(true)} className="mr-3" size={18} />
+                  }
+                  <span onClick={() => {
                     setCategoryName(cat_title);
                     setSelectedCategory(cat_title); // Set the selected category
-                    setOpenSidebar(false)
-                  }}
-                  className={`text-ellipsis overflow-hidden lg:w-full whitespace-nowrap ${selectedCategory === cat_title ? 'bg-gray-100 text-black' : 'text-zinc-800 hover:bg-zinc-100 hover:text-zinc-950'} font-semibold flex cursor-pointer items-center lg:border-l-designColor py-2 px-4 font-medium text-gray-600 outline-none transition-all duration-100 ease-in-out lg:hover:border-l-4 lg:hover:border-l-designColor lg:hover:text-designColor focus:border-l-4  text-sm md:text-base`}>
-                  
-                  {
-                    selectedCategory === cat_title ? <MinusSquare className="mr-3" size={18}/> : <PlusSquare className="mr-3" size={18}/>
-                  }
-                 
-                  {cat_title?.charAt(0).toUpperCase() + cat_title?.slice(1)}
+                    setOpenSidebar(false);
+                    setIsOpen(true)
+                  }}>
+
+                    {cat_title?.charAt(0).toUpperCase() + cat_title?.slice(1)}
+                  </span>
 
                 </button>
                 <ol className="w-[250px]">
-                  { selectedCategory === cat_title &&
-                    products.map(product => 
-                      <Link  href={{
+                  {selectedCategory === cat_title && isOpen &&
+                    products.map(product =>
+                      <Link href={{
                         pathname: `/id_${product?.id}`,
                         query: {
                           id: product?.id,
                           prefix: product?.category,
                         },
                       }} className="hover:bg-zinc-100 hover:text-zinc-950 py-2 flex pl-3 mb-1 ml-6 items-center gap-1 w-full justify-start">
-                      <ChevronRight size={15}/>
-                    <li className="w-[200px] text-ellipsis whitespace-nowrap overflow-hidden font-semibold">{product.title}</li>
-                  </Link>)
+                        <ChevronRight size={15} />
+                        <li className="w-[200px] text-ellipsis whitespace-nowrap overflow-hidden font-semibold">{product.title}</li>
+                      </Link>)
                   }
 
 
