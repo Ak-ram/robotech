5e3ea6ec09f71abb6dd86b5e416d32a708bbdd16
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-02-04 11:09:02 +0000

    updates

--- src/components/ApexChart.tsx
+++ src/components/ApexChart.tsx
@@ -1,5 +1,54 @@
+// import React, { useState, useEffect } from 'react';
+// import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+// const ApexChart = ({ categoryStats }) => {
+//   const [chartData, setChartData] = useState([]);
+
+//   useEffect(() => {
+//     if (categoryStats) {
+//       const data = categoryStats.map((category) => ({
+//         name: abbreviateName(category.categoryName),
+//         fullName: category.categoryName, // Full name for tooltip
+//         value: category.inStockLength,
+//       }));
+
+//       setChartData(data);
+//     }
+//   }, [categoryStats]);
+
+//   // Function to abbreviate the category name
+//   const abbreviateName = (fullName) => {
+//     // Your abbreviation logic goes here
+//     // For simplicity, let's just use the first two characters
+//     return fullName.substring(0, 2).toUpperCase();
+//   };
+
+//   return (
+//     <div>
+//       <BarChart width={400} height={200} data={chartData}>
+//         <CartesianGrid strokeDasharray="1 1" />
+//         <XAxis dataKey="name" angle={-60} textAnchor="end" interval={0} />
+//         <YAxis />
+//         <Tooltip formatter={(value, name, props) => [value, props.payload.fullName]} />
+//         <Legend
+//           formatter={(value, entry, index) => (
+//             <span style={{ color: entry.color }}>In Stock Qunatity</span>
+//           )}
+//           align="right"
+//           verticalAlign="top"
+//           height={36}
+//         />
+//         <Bar dataKey="value" fill="#1a73e8" label={{ position: 'top', fill: '#1a73e8' }} />
+//       </BarChart>
+//     </div>
+//   );
+// };
+
+// export default ApexChart;
+
+
 import React, { useState, useEffect } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const ApexChart = ({ categoryStats }) => {
   const [chartData, setChartData] = useState([]);
@@ -8,7 +57,7 @@ const ApexChart = ({ categoryStats }) => {
     if (categoryStats) {
       const data = categoryStats.map((category) => ({
         name: abbreviateName(category.categoryName),
-        fullName: category.categoryName, // Full name for tooltip
+        fullName: category.categoryName,
         value: category.inStockLength,
       }));
 
@@ -16,30 +65,27 @@ const ApexChart = ({ categoryStats }) => {
     }
   }, [categoryStats]);
 
-  // Function to abbreviate the category name
   const abbreviateName = (fullName) => {
-    // Your abbreviation logic goes here
-    // For simplicity, let's just use the first two characters
     return fullName.substring(0, 2).toUpperCase();
   };
 
   return (
     <div>
-      <BarChart width={600} height={400} data={chartData}>
+      <LineChart className='w-full' width={500} height={380} data={chartData}>
         <CartesianGrid strokeDasharray="1 1" />
         <XAxis dataKey="name" angle={-60} textAnchor="end" interval={0} />
         <YAxis />
         <Tooltip formatter={(value, name, props) => [value, props.payload.fullName]} />
-        <Legend
+        {/* <Legend
           formatter={(value, entry, index) => (
-            <span style={{ color: entry.color }}>In Stock Qunatity</span>
+            <span style={{ color: entry.color }}>In Stock Quantity</span>
           )}
           align="right"
           verticalAlign="top"
           height={36}
-        />
-        <Bar dataKey="value" fill="#1a73e8" label={{ position: 'top', fill: '#1a73e8' }} />
-      </BarChart>
+        /> */}
+        <Line type="monotone" dataKey="value" stroke="#1a73e8" label={{ position: 'top', fill: '#1a73e8' }} />
+      </LineChart>
     </div>
   );
 };
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -6,6 +6,7 @@ import { ProductType } from "../../type";
 import FormattedPrice from "./FormattedPrice";
 import Products from "./Products";
 import ApexChartComp from "./ApexChart";
+import { BarChart, LineChart, X } from "lucide-react";
 
 interface Product {
     id: string;
@@ -40,6 +41,7 @@ const Stats = () => {
     const [categoryStats, setCategoryStats] = useState<CategoryStats[]>([]);
     const [products, setProducts] = useState<string[]>([]);
     const [categories, setCategories] = useState<string[]>([]);
+    const [isShow, setIsShow] = useState<boolean>(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -90,7 +92,111 @@ const Stats = () => {
     return (
         <>
 
+            <section className='flex'>
+                <div className={`${isShow ? "block" : "hidden"} fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center`}>
+                    <div className={`bg-white min-w-[40rem] p-8 rounded-lg shadow-md`}>
+                        <div className="flex-1"> <X className="cursor-pointer" onClick={() => setIsShow(false)} /> <ApexChartComp categoryStats={categoryStats} /></div>
+                    </div></div>
+                <div>
+                    <div className="flex mx-auto flex-col">
+                        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div className="py-2 align-middle inline-block lg:px-8">
+                                <div className="shadow overflow-hidden sm:rounded-lg">
+                                    <div className="bg-gray-800 p-3 border-b  border-gray-700">
+                                        <span onClick={() => setIsShow(true)} className="text-slate-400 hover:text-white cursor-pointer py-1 rounded w-10 h-10 flex items-center justify-center  hover:bg-gray-700 block">
 
+                                            <LineChart className="" />
+                                        </span>
+                                    </div>
+                                    <table className="text-sm text-gray-400">
+                                        <thead className="bg-gray-800 text-xs uppercase ">
+                                            <tr>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    Category
+                                                </th>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    Quantity
+                                                </th>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    In Stock
+                                                </th>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    IS Price
+                                                </th>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    Out Stock
+                                                </th>
+                                                <th scope="col" className="text-center text-xs  py-3 text-left tracking-wider">
+                                                    OS Price
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-gray-800">
+                                            {categoryStats.map((categoryInfo, index) => (
+                                                <tr key={index} className=" bg-black bg-opacity-20 ">
+                                                    <td className="text-center pl-3 text-xs  flex  py-2 whitespace-nowrap">
+                                                        <img className="w-6 h-6 mr-2 rounded" src={categoryInfo.inStockProducts[0].image1} alt="" />
+                                                        <span className="text-xs font-medium">{categoryInfo.categoryName.slice(0, 1).toUpperCase() + categoryInfo.categoryName.slice(1,)}</span>
+                                                    </td>
+                                                    <td className="text-center text-xs   py-2 whitespace-nowrap">{categoryInfo.quantity}</td>
+                                                    <td className={`${categoryInfo.inStockLength >= 10 ? "text-green-500" :
+                                                        categoryInfo.inStockLength > 5 ? 'text-orange-400' :
+                                                            'text-yellow-500'
+                                                        } text-center text-xs   py-2 whitespace-nowrap`}>{categoryInfo.inStockLength}</td>
+                                                    <td className="text-center text-xs   py-2 whitespace-nowrap">
+                                                        <FormattedPrice className="text-xs" amount={categoryInfo.inStockTotalPrice} />
+                                                    </td>
+                                                    <td className={`${categoryInfo.outStockLength ? "text-red-500" : ""} text-center text-xs text-sm  py-2 whitespace-nowrap`}>{categoryInfo.outStockLength}</td>
+                                                    <td className="text-center text-xs  py-2 whitespace-nowrap">
+                                                        <FormattedPrice className="text-xs" amount={categoryInfo.outStockTotalPrice} /></td>
+                                                </tr>
+                                            ))}
+                                            <tr>
+
+                                                <th scope="col" className="text-xs pl-3 py-2 text-left tracking-wider">
+                                                    Total
+                                                </th>
+                                                <th scope="col" className="text-center text-xs   py-3 text-left tracking-wider">
+                                                    {products?.length}
+                                                </th>
+                                                <th scope="col" className="text-center text-xs   py-3 text-left tracking-wider">
+                                                    {
+                                                        categoryStats
+                                                            .map(item => item.inStockProducts.map((product: ProductType) => +product?.count !== 0))
+                                                            .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
+                                                    }                                            </th>
+                                                <th scope="col" className="text-center text-xs   py-3 text-left tracking-wider">
+                                                    <FormattedPrice className="text-xs" amount=
+                                                        {
+                                                            categoryStats
+                                                                .map(item => item.inStockProducts.map((product: ProductType) => +product?.price))
+                                                                .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
+                                                        } />
+                                                </th>
+                                                <th scope="col" className="text-center text-xs   px-6 py-3 text-left tracking-wider">
+                                                    {
+                                                        categoryStats
+                                                            .map(item => item.outStockProducts.map((product: ProductType) => +product?.count === 0))
+                                                            .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
+                                                    }
+                                                </th>
+                                                <th scope="col" className="text-center text-xs px-6 py-3 text-left tracking-wider">
+                                                    <FormattedPrice className="text-xs" amount=
+                                                        {
+                                                            categoryStats
+                                                                .map(item => item.outStockProducts.map((product: ProductType) => +product?.price))
+                                                                .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
+                                                        } />
+                                                </th>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+                        </div></div>
+                </div>
+            </section>
 
             <div className="flex flex-col items-center min-h-screen py-10">
                 <h1 className="text-lg text-gray-950 font-medium">Stats For {
@@ -100,105 +206,11 @@ const Stats = () => {
                 }</h1>
 
 
-                <ApexChartComp categoryStats={categoryStats} />
+
                 {/* <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />  */}
 
 
-                <div className="flex w-[90%] mx-auto flex-col mt-6">
-                    <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                            <div className="shadow overflow-hidden sm:rounded-lg">
-                                <table className="min-w-full text-sm text-gray-400">
-                                    <thead className="bg-gray-800 text-xs uppercase font-medium">
-                                        <tr>
-                                            <th></th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                Category
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                Quantity
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                In Stock
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                IS Price
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                Out Stock
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                OS Price
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="bg-gray-800">
-                                        {categoryStats.map((categoryInfo, index) => (
-                                            <tr key={index} className="bg-black bg-opacity-20 ">
-                                                <td className="text-center pl-4">{index + 1}</td>
-                                                <td className="text-center flex px-6 py-4 whitespace-nowrap">
-                                                    <img className="w-6 h-6 rounded" src={categoryInfo.inStockProducts[0].image1} alt="" />
-                                                    <span className="ml-2 font-medium">{categoryInfo.categoryName.slice(0, 1).toUpperCase() + categoryInfo.categoryName.slice(1,)}</span>
-                                                </td>
-                                                <td className="text-center px-6 py-4 whitespace-nowrap">{categoryInfo.quantity}</td>
-                                                <td className={`${categoryInfo.inStockLength >= 10 ? "text-green-500" :
-                                                    categoryInfo.inStockLength > 5 ? 'text-orange-400' :
-                                                        'text-yellow-500'
-                                                    } text-center px-6 py-4 whitespace-nowrap`}>{categoryInfo.inStockLength}</td>
-                                                <td className="text-center px-6 py-4 whitespace-nowrap">
-                                                    <FormattedPrice amount={categoryInfo.inStockTotalPrice} />
-                                                </td>
-                                                <td className={`${categoryInfo.outStockLength ? "text-red-500" : ""} text-center px-6 py-4 whitespace-nowrap`}>{categoryInfo.outStockLength}</td>
-                                                <td className="text-center px-6 py-4 whitespace-nowrap">
-                                                    <FormattedPrice amount={categoryInfo.outStockTotalPrice} /></td>
-                                            </tr>
-                                        ))}
-                                        <tr>
-                                            <th></th>
 
-                                            <th scope="col" className="px-6 py-3 text-left tracking-wider">
-                                                Total
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                {products?.length}
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                {
-                                                    categoryStats
-                                                        .map(item => item.inStockProducts.map((product: ProductType) => +product?.count !== 0))
-                                                        .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
-                                                }                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                <FormattedPrice amount=
-                                                    {
-                                                        categoryStats
-                                                            .map(item => item.inStockProducts.map((product: ProductType) => +product?.price))
-                                                            .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
-                                                    } />
-                                            </th>
-                                            <th scope="col" className="text-center  px-6 py-3 text-left tracking-wider">
-                                                {
-                                                    categoryStats
-                                                        .map(item => item.outStockProducts.map((product: ProductType) => +product?.count === 0))
-                                                        .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
-                                                }
-                                            </th>
-                                            <th scope="col" className="text-center px-6 py-3 text-left tracking-wider">
-                                                <FormattedPrice amount=
-                                                    {
-                                                        categoryStats
-                                                            .map(item => item.outStockProducts.map((product: ProductType) => +product?.price))
-                                                            .reduce((accumulator, currentValue) => accumulator + currentValue.reduce((a, b) => a + b, 0), 0)
-                                                    } />
-                                            </th>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </>
     );
