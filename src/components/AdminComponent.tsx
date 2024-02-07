142851453edb7c196c429ef352c136600efe1f0b
akram2-ashraf2 <159114042+akram2-ashraf2@users.noreply.github.com>
2024-02-07 16:29:51 +0000

    updates

--- src/components/AdminComponent.tsx
+++ src/components/AdminComponent.tsx
@@ -1,17 +1,130 @@
-"use client";
-import Container from "@/components/Container";
-import { useRouter } from "next/navigation";
-import React, { ReactComponentElement, useEffect, useState } from "react";
+// "use client";
+// import Container from "@/components/Container";
+// import { useRouter } from "next/navigation";
+// import React, { ReactComponentElement, useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import { StateProps } from "../../type";
+// import AdminProducts from "./AdminProducts";
+// import Print3DComponent from "./Admin3DComponent";
+// import AdminCourses from "./AdminCourses";
+// import Admin3DComponent from "./Admin3DComponent";
+// import AdminAbout from "./AdminAbout";
+// import AdminFaq from "./AdminFaq";
+// import AdminSlides from "./AdminSlides";
+// import AdminAnnouncement from "./AdminAnnouncement";
+// import { BarChart, GraduationCap, HelpCircle, Key, LayoutList, Menu, Printer, SlidersHorizontal, Smile, StickyNote, UserCircle, X } from "lucide-react";
+// import Stats from "./Stats";
+// import AdminCustomers from "./AdminCustomers";
+
+// const AdminComponent = () => {
+//   const userInfo = useSelector((state: StateProps) => state.pro.userInfo);
+//   interface SidebarItem {
+//     id: number;
+//     label: string;
+//     content: any;
+//     icon: any;
+//   }
+//   const [selectedItem, setSelectedItem] = useState<SidebarItem | null>(null);
+//   const [isOpen, setOpen] = useState<Boolean>(true);
+//   const sidebarItems: SidebarItem[] = [
+//     { id: 1, icon: <LayoutList />, label: "Products", content: <AdminProducts /> },
+//     { id: 2, icon: <Printer />, label: "3D", content: <Admin3DComponent /> },
+//     { id: 3, icon: <GraduationCap />, label: "Courses", content: <AdminCourses /> },
+//     { id: 4, icon: <Smile />, label: "About", content: <AdminAbout /> },
+//     { id: 5, icon: <HelpCircle />, label: "Faq", content: <AdminFaq /> },
+//     { id: 6, icon: <SlidersHorizontal />, label: "Slides", content: <AdminSlides /> },
+//     { id: 7, icon: <StickyNote />, label: "Announcement", content: <AdminAnnouncement /> },
+//     { id: 8, icon: <UserCircle />, label: "Customers", content: <AdminCustomers /> },
+//     { id: 9, icon: <BarChart />, label: "Stats", content: <Stats /> },
+//     // Add more items as needed
+//   ];
+//   const router = useRouter();
+
+//   const handleItemClick = (item: SidebarItem) => {
+//     setSelectedItem(item);
+//   };
+//   // Check authentication status without redirecting
+//   if (!userInfo) {
+//     router.push('/login')
+//     return <h2>You should login first, you will be redirected to login page...</h2>; // Or render a login component, redirect, or some other behavior
+//   }
+
+//   // Check if the user has the right authorization
+//   const isAuthorized =
+//     userInfo.email === process.env.NEXT_PUBLIC_AUTH_USERNAME &&
+//     userInfo.password === process.env.NEXT_PUBLIC_AUTH_PASSWORD;
+
+//   if (!isAuthorized) {
+//     return null; // Or render a login component, redirect, or some other behavior
+//   }
+
+//   return (
+//     <>
+//       {/* Sidebar */}
+//       <div className={`select-none bg-white text-black transition-all ${isOpen ? 'w-[250px] py-4 px-2' : 'w-0'}  lg:border-r lg:border-gray-200 `}>
+//         {/* <h2 className="text-2xl font-bold mb-4">Pages</h2> */}
+
+//         <ul className="bg-white py-2  font-bold justify-between overflow-x-auto  lg:bg-transparent lg:items-start">
+//           {sidebarItems.map((item) => (
+//             <li
+//               key={item.id}
+//               className={`flex items-center gap-2 my-2 w-full hover:bg-slate-300 p-1.5 rounded-md cursor-pointer ${selectedItem === item ? "bg-slate-300" : ""
+//                 } my-0`}
+//               onClick={() => handleItemClick(item)}
+//             >
+//               {item.icon}
+//               {item.label}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       {/* Main content */}
+//       <div className="flex-1 py-5 p-4 ">
+//         {/* <header className="flex items-center justify-between mb-8"> */}
+//         {/* <h2 className="text-2xl font-bold">Robotech Panel</h2>
+//         </header> */}
+
+//         {selectedItem ? (
+//           <div className="">
+
+//             <div className="border-b-zinc-300 pb-2 border-b mb-2 flex items-center gap-2">
+//               <span className="hover:bg-slate-300 bg-white  py-1 px-1 rounded cursor-pointer w-fit inline-block" onClick={() => setOpen(!isOpen)}>
+//                 {
+//                   isOpen ? <X /> : <Menu />
+//                 }
+//               </span>
+//               <span className="bg-white hover:bg-slate-300 font-bold px-1.5 py-1 rounded">{selectedItem.label} Page</span>
+//             </div>
+//             {
+//               selectedItem.label
+//             }
+//           </div>
+//         ) : (
+//           <div>
+//             <span className="cursor-pointer w-fit inline-block" onClick={() => setOpen(!isOpen)}>
+//               {
+//                 isOpen ? <X /> : <Menu />
+//               }
+
+
+//             </span>
+//             <p className="font-bold text-lg flex items-center justify-center lg:h-[500px] mb-5 text-gray-600">
+
+//               Select Page from the sidebar.
+//             </p>
+//           </div>
+//         )}
+//       </div>
+//     </>
+
+
+//   );
+// };
+
+// export default AdminComponent;
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { StateProps } from "../../type";
-import AdminProducts from "./AdminProducts";
-import Print3DComponent from "./Admin3DComponent";
-import AdminCourses from "./AdminCourses";
-import Admin3DComponent from "./Admin3DComponent";
-import AdminAbout from "./AdminAbout";
-import AdminFaq from "./AdminFaq";
-import AdminSlides from "./AdminSlides";
-import AdminAnnouncement from "./AdminAnnouncement";
 import { BarChart, GraduationCap, HelpCircle, Key, LayoutList, Menu, Printer, SlidersHorizontal, Smile, StickyNote, UserCircle, X } from "lucide-react";
 import Stats from "./Stats";
 import AdminCustomers from "./AdminCustomers";
@@ -26,30 +139,44 @@ const AdminComponent = () => {
   }
   const [selectedItem, setSelectedItem] = useState<SidebarItem | null>(null);
   const [isOpen, setOpen] = useState<Boolean>(true);
+  const [password, setPassword] = useState<string>("");
+  const [isPasswordEntered, setIsPasswordEntered] = useState<boolean>(false);
   const sidebarItems: SidebarItem[] = [
-    { id: 1, icon: <LayoutList />, label: "Products", content: <AdminProducts /> },
-    { id: 2, icon: <Printer />, label: "3D", content: <Admin3DComponent /> },
-    { id: 3, icon: <GraduationCap />, label: "Courses", content: <AdminCourses /> },
-    { id: 4, icon: <Smile />, label: "About", content: <AdminAbout /> },
-    { id: 5, icon: <HelpCircle />, label: "Faq", content: <AdminFaq /> },
-    { id: 6, icon: <SlidersHorizontal />, label: "Slides", content: <AdminSlides /> },
-    { id: 7, icon: <StickyNote />, label: "Announcement", content: <AdminAnnouncement /> },
+    { id: 1, icon: <LayoutList />, label: "Products", content: <div>AdminProducts</div> },
+    { id: 2, icon: <Printer />, label: "3D", content: <div>Admin3DComponent</div> },
+    { id: 3, icon: <GraduationCap />, label: "Courses", content: <div>AdminCourses</div> },
+    { id: 4, icon: <Smile />, label: "About", content: <div>AdminAbout</div> },
+    { id: 5, icon: <HelpCircle />, label: "Faq", content: <div>AdminFaq</div> },
+    { id: 6, icon: <SlidersHorizontal />, label: "Slides", content: <div>AdminSlides</div> },
+    { id: 7, icon: <StickyNote />, label: "Announcement", content: <div>AdminAnnouncement</div> },
     { id: 8, icon: <UserCircle />, label: "Customers", content: <AdminCustomers /> },
     { id: 9, icon: <BarChart />, label: "Stats", content: <Stats /> },
     // Add more items as needed
   ];
-  const router = useRouter();
 
   const handleItemClick = (item: SidebarItem) => {
     setSelectedItem(item);
   };
-  // Check authentication status without redirecting
+
+  const handlePasswordSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Check if the entered password matches the pre-defined password
+    const preDefinedPassword = "your_pre_defined_password";
+    if (password === preDefinedPassword) {
+      setIsPasswordEntered(true);
+    }
+  };
+
   if (!userInfo) {
-    router.push('/login')
-    return <h2>You should login first, you will be redirected to login page...</h2>; // Or render a login component, redirect, or some other behavior
+    return (
+      <>
+        <h2>You should login first, you will be redirected to the login page...</h2>
+        {/* Redirect to the login page */}
+      </>
+    );
   }
 
-  // Check if the user has the right authorization
   const isAuthorized =
     userInfo.email === process.env.NEXT_PUBLIC_AUTH_USERNAME &&
     userInfo.password === process.env.NEXT_PUBLIC_AUTH_PASSWORD;
@@ -60,16 +187,12 @@ const AdminComponent = () => {
 
   return (
     <>
-      {/* Sidebar */}
       <div className={`select-none bg-white text-black transition-all ${isOpen ? 'w-[250px] py-4 px-2' : 'w-0'}  lg:border-r lg:border-gray-200 `}>
-        {/* <h2 className="text-2xl font-bold mb-4">Pages</h2> */}
-
         <ul className="bg-white py-2  font-bold justify-between overflow-x-auto  lg:bg-transparent lg:items-start">
           {sidebarItems.map((item) => (
             <li
               key={item.id}
-              className={`flex items-center gap-2 my-2 w-full hover:bg-slate-300 p-1.5 rounded-md cursor-pointer ${selectedItem === item ? "bg-slate-300" : ""
-                } my-0`}
+              className={`flex items-center gap-2 my-2 w-full hover:bg-slate-300 p-1.5 rounded-md cursor-pointer ${selectedItem === item ? "bg-slate-300" : ""} my-0`}
               onClick={() => handleItemClick(item)}
             >
               {item.icon}
@@ -78,44 +201,44 @@ const AdminComponent = () => {
           ))}
         </ul>
       </div>
-      {/* Main content */}
       <div className="flex-1 py-5 p-4 ">
-        {/* <header className="flex items-center justify-between mb-8"> */}
-        {/* <h2 className="text-2xl font-bold">Robotech Panel</h2>
-        </header> */}
-
         {selectedItem ? (
           <div className="">
-
             <div className="border-b-zinc-300 pb-2 border-b mb-2 flex items-center gap-2">
               <span className="hover:bg-slate-300 bg-white  py-1 px-1 rounded cursor-pointer w-fit inline-block" onClick={() => setOpen(!isOpen)}>
-                {
-                  isOpen ? <X /> : <Menu />
-                }
+                {isOpen ? <X /> : <Menu />}
               </span>
               <span className="bg-white hover:bg-slate-300 font-bold px-1.5 py-1 rounded">{selectedItem.label} Page</span>
             </div>
-            <div>{selectedItem.content}</div>
+            {isPasswordEntered ? (
+              selectedItem.content
+
+            ) : (
+              <div>
+                <form onSubmit={handlePasswordSubmit}>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter the password"
+                  />
+                  <button type="submit">Submit</button>
+                </form>
+              </div>
+            )}
           </div>
         ) : (
           <div>
             <span className="cursor-pointer w-fit inline-block" onClick={() => setOpen(!isOpen)}>
-              {
-                isOpen ? <X /> : <Menu />
-              }
-
-
+              {isOpen ? <X /> : <Menu />}
             </span>
             <p className="font-bold text-lg flex items-center justify-center lg:h-[500px] mb-5 text-gray-600">
-
               Select Page from the sidebar.
             </p>
           </div>
         )}
       </div>
     </>
-
-
   );
 };
 
