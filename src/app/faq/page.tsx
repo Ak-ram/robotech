3103dd8e29aed8d5eb3458977e0dc809676149fb
akram2-ashraf2 <159114042+akram2-ashraf2@users.noreply.github.com>
2024-02-07 15:16:02 +0000

    updates

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -6,14 +6,10 @@ import React from "react";
 const page = async () => {
   // const products = await getFaq();
   return (
-    <Container className="py-10">
-      <div className="border-b-[1px] border-b-zinc-400 pb-4 flex items-center justify-between">
-        <h2 className="font-bold text-2xl">FAQ</h2>
-      </div>
+    
 
       <SupportComponent />
 
-    </Container>
   );
 };
 
--- src/components/SupportComponent.tsx
+++ src/components/SupportComponent.tsx
@@ -1,9 +1,8 @@
 'use client'
-// Import the required types
 import React, { useState, useEffect } from 'react';
 import faq from '@/assets/Faq.png';
 import Image from 'next/image';
-import { ChevronDown } from 'lucide-react';
+import { Activity, ChevronDown } from 'lucide-react';
 import { getFaq } from '@/helpers/getFaq';
 
 // Define the type for your FAQ item
@@ -41,48 +40,74 @@ function SupportComponent() {
   };
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-white rounded py-1 mt-3">
       <div>
-        <div className="mt-8 opacity-75">
-          <Image className="m-auto" src={faq} alt="faq" width={600} height={400} />
+        <div className="flex items-center justify-center gap-2 mx-auto text-center px-4 mt-8 text-2xl text-indigo-900 font-semibold">
+          <Activity className='bg-slate-100/80 w-12 h-12 p-2 rounded-full text-rose-400' size={30} /> Frequently Asked Questions
         </div>
-        <div>
-          <div className="mx-auto text-center px-4 mt-8 text-2xl text-indigo-900 font-semibold">
-            Frequently Asked Questions
-          </div>
-          <dl style={{direction: 'rtl'}} className="mt-8 mx-auto max-w-screen-sm lg:max-w-screen-lg flex flex-col lg:flex-row lg:flex-wrap">
+        <dl style={{ direction: 'rtl' }} className="mt-8 mx-auto max-w-screen-sm lg:max-w-screen-lg flex flex-col lg:flex-row lg:flex-wrap">
+          <div className="lg:w-1/2" >
             {data &&
-              data.map((item, i) => (
-                <div className="lg:w-1/2" key={`${i}_${item?.question}`}>
-                  <div
-                    className="question-and-answer select-none cursor-pointer border-2 mx-8 my-3 px-6 py-4 rounded-lg group"
-                    onClick={() => toggleAnswer(i)}
-                  >
-                    <dt className="">
-                      <div className="flex justify-between items-center">
-                        <div className="text-indigo-800 font-semibold cursor-pointer">
-                          {item?.question}
-                        </div>
-                        <div>
-                          <ChevronDown size={16} />
-                        </div>
+              data.slice(0, Math.ceil(data?.length / 2)).map((item, i) => (
+                <div
+                  className="question-and-answer select-none cursor-pointer border-2 mx-8 my-3 rounded-lg group"
+                  onClick={() => toggleAnswer(i)}
+                  key={`${i}_${item?.question}`}
+                >
+                  <dt className={`${item.open ? "bg-white" : "hover:bg-slate-100"} py-3 px-2 rounded`}>
+                    <div className="flex justify-between items-center">
+                      <div className="text-indigo-800 font-semibold cursor-pointer">
+                        {item?.question}
                       </div>
-                    </dt>
-                    <dd
-                      className={`answer mt-2 leading-snug text-gray-700 ${!item.open
-                        ? 'max-h-0 overflow-hidden transition-max-height duration-300'
-                        : 'max-h-screen border-t pt-3 transition-max-height duration-300'
-                        }`}
-                    >
-                      {item?.answer}
-                    </dd>
-                  </div>
+                      <div>
+                        <ChevronDown size={16} />
+                      </div>
+                    </div>
+                  </dt>
+                  <dd
+                    className={`answer bg-white rounded text-gray-700 ${!item.open
+                      ? 'h-0 overflow-hidden  duration-300'
+                      : 'h-fit  py-3 px-2 border-t -max-height duration-300'
+                      }`}
+                  >
+                    {item?.answer}
+                  </dd>
                 </div>
               ))}
-          </dl>
-        </div>
+          </div>
+          <div className="lg:w-1/2" >
+            {data &&
+              data.slice(Math.ceil(data?.length / 2), data?.length).map((item, i) => (
+                <div
+                  className="question-and-answer select-none cursor-pointer border-2 mx-8 my-3 rounded-lg group"
+                  onClick={() => toggleAnswer(i + Math.ceil(data?.length / 2))}
+                  key={`${i}_${item?.question}`}
+                >
+                  <dt className={`${item.open ? "bg-white" : "hover:bg-slate-100"} py-3 px-2 rounded`}>
+                    <div className="flex justify-between items-center">
+                      <div className="text-indigo-800 font-semibold cursor-pointer">
+                        {item?.question}
+                      </div>
+                      <div>
+                        <ChevronDown size={16} />
+                      </div>
+                    </div>
+                  </dt>
+                  <dd
+                    className={`answer bg-white rounded text-gray-700 ${!item.open
+                      ? 'h-0 overflow-hidden  duration-300'
+                      : 'h-fit  py-3 px-2 border-t -max-height duration-300'
+                      }`}
+                  >
+                    {item?.answer}
+                  </dd>
+                </div>
+              ))}
+          </div>
+        </dl>
       </div>
     </div>
+
   );
 }
 
