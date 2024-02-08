23d0a26ec3c8f460b480389742d7fa64810c1fc7
akram2-ashraf2 <159114042+akram2-ashraf2@users.noreply.github.com>
2024-02-08 08:38:52 +0000

    updates

--- src/app/[prefix]_[id]/page.tsx
+++ src/app/[prefix]_[id]/page.tsx
@@ -224,38 +224,40 @@ const Page: React.FC<Props> = ({ searchParams }: Props) => {
                   <div className="mt-8 flow-root sm:mt-12">
                     <h1 className="text-lg md:text-3xl font-bold">More details about {product?.title}</h1>
 
-                      <ul className="space-y-1 font-semibold text-gray-600 mb-6">
+                      {product?.description && (
+                    <ul className="space-y-1 font-semibold mt-3 text-gray-600 mb-6">
+                      <p className="mb-5">{product?.description?.split('|').slice(0,1)}</p>
+                        <span className="font-bold text-black">Product Attributes : </span>
 
-                        {product?.description && (
-                          <>
-                            {product?.description?.split('|').map((part, index) => {
-  // Check if the part contains bullet points
-  if (part.includes('|')) {
-    // If it contains bullet points, split it further based on '|'
-    const bullets = part.split('|').map(bullet => bullet.trim());
-    return bullets.map((bullet, i) => (
-      <li key={i} className="flex items-center px-2 sm:px-6 py-2.5 hover:bg-gray-100 transition-colors duration-300 ease-in-out">
-        <span className="w-[5px] h-[5px] mr-2 rounded-full bg-black"></span>
-        <span>{bullet}</span>
-      </li>
-    ));
-  } else {
-    // If it doesn't contain bullet points, render as ordinary text
-    return (
-      <p className="mt-4 text-xs sm:text-base items-center flex gap-1" key={index}>
-        <span className="w-[5px] h-[5px] mr-2 rounded-full bg-black"></span>
-        <span>{part.split('|')[0]}</span>
-      </p>
-    );
-  }
-})}
+                        <>
+                          {product?.description?.split('|').slice(1).map((part, index) => {
+                            // Check if the part contains bullet points
+                            if (part.includes('|')) {
+                              // If it contains bullet points, split it further based on '|'
+                              const bullets = part.split('|').map(bullet => bullet.trim());
+                              return bullets.map((bullet, i) => (
+                                <li key={i} className="flex items-center px-2 sm:px-6 py-2.5 hover:bg-gray-100 transition-colors duration-300 ease-in-out">
+                                  <span className="w-[5px] h-[5px] mr-2 rounded-full bg-black"></span>
+                                  <span>{bullet}</span>
+                                </li>
+                              ));
+                            } else {
+                              // If it doesn't contain bullet points, render as ordinary text
+                              return (
+                                <p className="mt-4 text-xs sm:text-base items-center flex gap-1" key={index}>
+                                  <span className="w-[5px] h-[5px] mr-2 rounded-full bg-black"></span>
+                                  <span>{part.split('|')[0]}</span>
+                                </p>
+                              );
+                            }
+                          })}
 
-                          </>
-                        )}
+                        </>
+                    </ul>
+                      )}
 
-                      </ul>
 
-                  
+
                     <h1 className="mt-8 text-lg md:text-3xl  font-bold">Quick Order ?</h1>
                     <p className="mt-4 text-xs sm:text-base">Contact Us in Whatsapp : 01066745733</p>
                   </div>
