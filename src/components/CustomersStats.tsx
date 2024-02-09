4d8a975368c8d38b13cb04b7acd217d91cabae11
akram2-ashraf2 <159114042+akram2-ashraf2@users.noreply.github.com>
2024-02-09 20:22:13 +0000

    Optimize

--- src/components/CustomersStats.tsx
+++ src/components/CustomersStats.tsx
@@ -9,6 +9,7 @@ import { ProductType } from "../../type";
 import { getProducts } from "@/helpers/getProducts";
 import TransactionAnalyzer from "./TransactionAnalyzer";
 import CustomerStatsTopSelling from "./CustomerStatsTopSelling";
+import CustomerStatsOutStocks from "./CustomerStatsOutStocks";
 
 const CustomersStats = () => {
     const [isShow, setIsShow] = useState<boolean>(false);
@@ -16,14 +17,12 @@ const CustomersStats = () => {
     const [customers, setCustomers] = useState<any[]>([]);
     const [highestTotalPurchase, setHighestTotalPurchase] = useState<number | null>(null);
     const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
-    const [products, setProducts] = useState<ProductType[]>([])
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const categoriesList = await getCustomerData();
                 const allProducts = await getProducts();
                 sortCustomers(categoriesList, sortOrder); // Initial sorting based on sortOrder
-                setProducts(allProducts)
 
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -101,113 +100,7 @@ const CustomersStats = () => {
 
 
             <div className="flex flex-col">
-                <div className="flex gap-2 ">
-
-                    <div className="flex-1 bg-white my-5 px-3 py-6 rounded-lg shadow-md animate-fade-in">
-
-
-                        {/* Out-Stocks Heading */}
-                        <h2 className="text-2xl font-semibold mb-6 flex items-center justify-center text-red-500 bg-red-100 py-2 px-4 rounded-md">
-                            <Ban className="mr-2 text-red-500" size={24} /> Out-Stocks
-
-                            <span className="mx-4 relative">
-                                <Search className="w-5 h-5 text-gray-500 absolute top-2 right-3" />
-                                <input
-                                    type="text"
-                                    placeholder="Product Name..."
-                                    value={searchQuery}
-                                    onChange={handleSearchInputChange}
-                                    className="pl-2 pr-10  placeholder-red-300 py-1 text-sm border border-red-200 rounded focus:outline-none focus:border-red-500"
-                                />
-
-                            </span>
-
-                            <span className="ml-auto text-sm">
-
-                                {products &&
-                                    products.filter((product) => +product?.count === 0).length}{' '}
-                                Product(s)
-                            </span>
-                        </h2>
-
-                        {/* Out-Stocks Product List */}
-                        <div className="mb-3 h-[380px] overflow-auto py-3 px-2 rounded-md">
-                            {products &&
-                                products
-                                    .filter((product) => +product?.count === 0)
-                                    .filter((product) =>
-                                        product.title.toLowerCase().includes(searchQuery.toLowerCase())
-                                    )
-                                    .map((product) => (
-                                        <div
-                                            className="flex items-center justify-start gap-1 border-b border-gray-300 py-2"
-                                            key={product.id}
-                                        >
-                                            <img
-                                                className="w-8 h-8 rounded"
-                                                src={product?.image1}
-                                                alt={product.title}
-                                            />{' '}
-                                            <div className="pl-3">{product.title}</div>
-                                        </div>
-                                    ))}
-                        </div>
-                    </div>
-
-
-                    <div className="flex-1 bg-white my-5 px-3 py-6 rounded-lg shadow-md animate-fade-in">
-
-
-                        {/* Out-Stocks Heading */}
-                        <h2 className="text-2xl font-semibold mb-6 flex items-center justify-center text-red-500 bg-red-100 py-2 px-4 rounded-md">
-                            <Ban className="mr-2 text-red-500" size={24} /> Out-Stocks
-
-                            <span className="mx-4 relative">
-                                <Search className="w-5 h-5 text-gray-500 absolute top-2 right-3" />
-                                <input
-                                    type="text"
-                                    placeholder="Product Name..."
-                                    value={searchQuery}
-                                    onChange={handleSearchInputChange}
-                                    className="pl-2 pr-10  placeholder-red-300 py-1 text-sm border border-red-200 rounded focus:outline-none focus:border-red-500"
-                                />
-
-                            </span>
-
-                            <span className="ml-auto text-sm">
-
-                                {products &&
-                                    products.filter((product) => +product?.count === 0).length}{' '}
-                                Product(s)
-                            </span>
-                        </h2>
-
-                        {/* Out-Stocks Product List */}
-                        <div className="mb-3 h-[380px] overflow-auto py-3 px-2 rounded-md">
-                            {products &&
-                                products
-                                    .filter((product) => +product?.count === 0)
-                                    .filter((product) =>
-                                        product.title.toLowerCase().includes(searchQuery.toLowerCase())
-                                    )
-                                    .map((product) => (
-                                        <div
-                                            className="flex items-center justify-start gap-1 border-b border-gray-300 py-2"
-                                            key={product.id}
-                                        >
-                                            <img
-                                                className="w-8 h-8 rounded"
-                                                src={product?.image1}
-                                                alt={product.title}
-                                            />{' '}
-                                            <div className="pl-3">{product.title}</div>
-                                        </div>
-                                    ))}
-                        </div>
-                    </div>
-
-                </div>
-
+                <CustomerStatsOutStocks />
 
 
 
