6b0f71e3a99ca5c20c9651a5087d80075abaffbe
akram2-ashraf2 <159114042+akram2-ashraf2@users.noreply.github.com>
2024-02-11 07:00:39 +0000

    updates

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,110 +1,103 @@
 import React from "react";
 import { FacebookShareButton, TwitterShareButton } from "react-share";
 import { FacebookIcon, TwitterIcon } from "react-share";
-import { PhoneCall, Check, Gift, Wallet2, Link2 } from "lucide-react"; // Import necessary Lucide icons
+import { PhoneCall, Check, Gift, Wallet2, Link2 } from "lucide-react";
 import FormattedPrice from "./FormattedPrice";
 import toast from "react-hot-toast";
 
 const ProductDetails = ({ product, prefix, dispatch, addToCart }) => {
     return (
-        <div className="lg:col-span-2 lg:row-span-2 lg:row-end-2">
-            <h1 className="sm:text-2xl font-bold text-gray-900 sm:text-3xl">{product?.title}</h1>
+        <div className="bg-white p-6 rounded lg:col-span-2 lg:row-span-2 lg:row-end-2">
+            <h1 className="text-3xl font-bold text-gray-900">{product?.title}</h1>
 
-            <div className="mt-5 flex flex-col items-stretch w-52 gap-2">
-                {/* Phone number */}
-                <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                    <PhoneCall size={18} className="text-green-600 mr-2" />
-                    <span className="font-semibold ml-1">Phone: 01102071544</span>
-                </span>
+            <div className="mt-5 border-t border-b py-4">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <p className="">
+                            {product?.price > 0 && prefix !== "print" ? (
+                                <>
+                                    <FormattedPrice className="text-xl font-bold" amount={product?.price!} />
+                                    <span className="text-xl font-bold"> / Piece</span>
+                                </>
+                            ) : (
+                                <>
+                                    <FormattedPrice className="text-xl font-bold" amount={product?.count!} />
+                                    <span className="text-xl font-bold"> / Minute</span>
+                                </>
+                            )}
+                        </p>
+                    </div>
+                    <button
+                        onClick={() => {
+                            dispatch(addToCart(product));
+                            toast.success(`${product?.title} successfully added to the basket`);
+                        }}
+                        type="button"
+                        className="inline-flex items-center justify-center px-6 py-3 text-sm font-bold text-white bg-gray-900 border border-transparent rounded-md shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-5 h-5 mr-2"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M5 10H2a1 1 0 00-1 1v2a1 1 0 001 1h3v3a1 1 0 001 1h2a1 1 0 001-1v-3h3a1 1 0 001-1v-2a1 1 0 00-1-1h-3V7a1 1 0 00-1-1H7a1 1 0 00-1 1v2zm3-8a1 1 0 00-1 1v-.001L7.707 6.293A1 1 0 008 7h2v1H8a3 3 0 01-3-3V2H2a1 1 0 00-1 1v2a1 1 0 001 1h1v6H2a1 1 0 00-1 1v2a1 1 0 001 1h1v1a3 3 0 003 3h4v2a1 1 0 001 1h2a1 1 0 001-1v-2h4a3 3 0 003-3v-1h1a1 1 0 001-1v-2a1 1 0 00-1-1h-1V9h1a1 1 0 001-1V6a1 1 0 00-1-1h-1V4a3 3 0 00-3-3H8z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        Add to cart
+                    </button>
+                </div>
 
-                {/* Social sharing buttons */}
-                <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                    <FacebookShareButton className="flex items-center" url={"productUrl"}>
-                        <FacebookIcon className="text-blue-500" size={18} />
-                        <span className="font-semibold ml-1">Share on Facebook</span>
-                    </FacebookShareButton>
-                </span>
-                <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                    <TwitterShareButton className="flex items-center" url={"productUrl"}>
-                        <TwitterIcon className="text-sky-500" size={18} />
-                        <span className="font-semibold ml-1">Share on Twitter</span>
-                    </TwitterShareButton>
-                </span>
+                <div className="mt-5 space-y-2">
+                    <div className="flex items-center text-sm font-medium text-gray-600">
+                        <Wallet2 className="w-4 h-4 mr-2" />
+                        Previous Price:{" "}
+                        <FormattedPrice className="mx-1"
+                            amount={product?.previousPrice!} /> from this product.
+                    </div>
+                    <div className="flex items-center text-sm font-medium text-gray-600">
+                        <Check className="w-4 h-4 mr-2" />
+                        You saved:{" "}
+                        <FormattedPrice
+                            className="mx-1"
+                            amount={product?.previousPrice! - product?.price!}
+                        />{" "}
+                        from this product.
+                    </div>
+                </div>
+            </div>
 
-                {/* External link */}
-                {product?.externalLink && product.externalLink.length &&
-                    <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                        <Link2 size={18} className="mr-2" />
+            <div className="mt-5 space-y-2">
+                <div className="flex items-center">
+                    <PhoneCall className="w-4 h-4 text-green-600 mr-2" />
+                    <span className="font-semibold">Phone: 01102071544</span>
+                </div>
+
+                <FacebookShareButton className="flex items-center" url={"productUrl"}>
+                    <FacebookIcon className="w-4 h-4 text-blue-500 mr-2" />
+                    <span className="font-semibold">Share on Facebook</span>
+                </FacebookShareButton>
+
+
+                <TwitterShareButton className="flex items-center" url={"productUrl"}>
+                    <TwitterIcon className="w-4 h-4 text-sky-500 mr-2" />
+                    <span className="font-semibold">Share on Twitter</span>
+                </TwitterShareButton>
+
+                {product?.externalLink && product.externalLink.length && (
+                    <div className="flex items-center">
+                        <Link2 className="w-4 h-4 text-blue-500 mr-2" />
                         <span className="font-semibold">
-                            <a href={product?.externalLink} className="text-blue-500 hover:underline">Visit</a>
+                            <a href={product?.externalLink} className="text-blue-500 hover:underline">
+                                Visit
+                            </a>
                         </span>
-                    </span>
-                }
-
-                {/* Product availability and status */}
-                {product?.count > 0 && prefix !== 'print' && (
-                    <>
-                        <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                            <span className="font-semibold mr-1">Availability:</span>
-                            <span className="flex items-center text-green-600">
-                                Available <Check className="ml-2" size={18} />
-                            </span>
-                        </span>
-                        <span className="hidden sm:flex bg-white rounded py-1 px-2 items-center">
-                            <span className="flex items-center text-blue-400">
-                                <Gift className="mr-2" size={20} />
-                            </span>
-                            <span className="font-semibold mr-1">Newly added</span>
-                        </span>
-                    </>
+                    </div>
                 )}
             </div>
-
-            {/* Product price and add to cart button */}
-            <div className="mt-10 flex lg:flex-col lg:items-start gap-4 items-center justify-between border-t border-b py-4 sm:flex-row sm:space-y-0">
-                <div className="flex items-end">
-                    <h1 className="sm:text-lg md:text-3xl font-bold">
-                        {product?.price > 0 && prefix !== 'print' ?
-                            <>
-                                <FormattedPrice amount={product?.price!} />
-                                <span className="text-base"> / Piece</span>
-                            </>
-                            :
-                            <>
-                                <FormattedPrice amount={product?.count!} />
-                                <span className="text-base"> / Minute</span>
-                            </>
-                        }
-                    </h1>
-                </div>
-                <button onClick={() => {
-                    dispatch(addToCart(product));
-                    toast.success(`${product?.title} successfully added to the basket`)
-                }} type="button" className="mt-0 inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-3 py-2 md:px-12 md:py-3 text-center text-sm sm:text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="shrink-0 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
-                    </svg>
-                    Add to cart
-                </button>
-            </div>
-
-            {/* Additional product information */}
-            <ul className="mt-8 space-y-2">
-                {product?.price < product?.previousPrice &&
-                    <>
-                        <li className="flex items-center text-left text-sm font-medium text-gray-600">
-                            <Wallet2 size={18} className="mr-1" />
-                            Previous Price:<span className="mr-1"></span> <FormattedPrice amount={product?.previousPrice!} /> <span className="ml-1"></span> from this product.
-                        </li>
-                        <li className="flex items-center text-left text-sm font-medium text-gray-600">
-                            <svg className="mr-2 block h-5 w-5 align-middle text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" className=""></path>
-                            </svg>
-                            You saved:<span className="mr-1"></span> <FormattedPrice amount={product?.previousPrice! - product?.price!} /> <span className="ml-1"></span> from this product.
-                        </li>
-                    </>
-                }
-            </ul>
         </div>
     );
 };
