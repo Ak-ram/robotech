c441038e44cb592455ea9fa7f5fe68372072892d
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-02-17 09:51:33 +0000

    updates

--- src/components/CustomSelectbox.tsx
+++ src/components/CustomSelectbox.tsx
@@ -1,8 +1,9 @@
+import { Edit } from 'lucide-react';
 import { useState } from 'react';
 
 // Define your component here
 
-const CustomSelect = ({ jsonData,selectedCat,setSelectedCat,setSelectedSectionIndex }) => {
+const CustomSelect = ({ jsonData, selectedCat, setSelectedCat, setSelectedSectionIndex }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -34,15 +35,14 @@ const CustomSelect = ({ jsonData,selectedCat,setSelectedCat,setSelectedSectionIn
 
       {/* Custom select box */}
       <div
-        className="relative flex flex-col items-start bg-white border border-gray-300 rounded-md p-2 space-y-2 cursor-pointer"
+        className="relative flex flex-col items-start bg-white border border-gray-300 rounded-md p-2 space-y-2 "
         onClick={() => setIsOpen(!isOpen)}
       >
-        <div className="flex justify-between items-center w-full">
+        <div className="cursor-pointer flex justify-between items-center w-full">
           <span>{selectedCat || "Select an option"}</span>
           <svg
-            className={`w-6 h-6 transition-transform ${
-              isOpen ? "transform rotate-180" : ""
-            }`}
+            className={`w-6 h-6 transition-transform ${isOpen ? "transform rotate-180" : ""
+              }`}
             fill="none"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -54,23 +54,23 @@ const CustomSelect = ({ jsonData,selectedCat,setSelectedCat,setSelectedSectionIn
           </svg>
         </div>
         {isOpen && (
-          <div className="absolute z-100 h-[200px] overflow-scroll w-full top-full left-0 bg-white border border-gray-300 rounded-b-md overflow-hidden shadow-md">
+          <div className="absolute z-100 h-[200px] text-start overflow-scroll w-full top-full left-0 bg-white border border-gray-300 rounded-b-md overflow-hidden shadow-md">
             {jsonData.flatMap((section, sectionIndex) =>
               Object.keys(section).map((item) => {
                 const newIndex = sectionIndex + 1;
                 return (
                   <div
                     key={`${newIndex}-${item}`}
-                    className={`p-2 ${
-                      selectedCat === item ? "bg-blue-500 text-white" : "hover:bg-gray-200"
-                    }`}
-                    onClick={() => {
+                    className={`p-2 gap-2 flex items-center ${selectedCat === item ? "bg-blue-500 text-white" : "hover:bg-gray-200"
+                      }`}
+              
+                  >
+                    <span className='flex-1 cursor-pointer' onClick={() => {
                       setSelectedCat(item);
                       setSelectedSectionIndex(newIndex);
                       setIsOpen(false); // Close the dropdown after selecting an option
-                    }}
-                  >
-                    {item}
+                    }}>{item}</span>
+                    <span className='cursor-pointer'><Edit size={15} /></span>
                   </div>
                 );
               })
