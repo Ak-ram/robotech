4a4f490e05f900bdcf3e37cd3ec1623c08ffe366
Akram-44 <151398447+Akram-44@users.noreply.github.com>
2024-02-27 11:32:15 +0000

    Update

--- src/components/Compare.tsx
+++ src/components/Compare.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductType, StateProps } from "../../type";
 import Image from "next/image";
@@ -13,10 +13,16 @@ import FavImg from "@/assets/fav.png"
 import Container from "./Container";
 const Compare = () => {
     const { compareData, favoriteData } = useSelector((state: StateProps) => state.pro);
+    const [expandedProduct, setExpandedProduct] = useState<string | null>(null);
+
     const dispatch = useDispatch();
 
     const isFavorite = (productId: any) => {
         return favoriteData.some((favoriteItem) => favoriteItem.id === productId);
+    };
+    // Toggle product description visibility
+    const toggleDescription = (productId: string) => {
+        setExpandedProduct(productId === expandedProduct ? null : productId);
     };
     return (
         <Container>
@@ -30,7 +36,6 @@ const Compare = () => {
                             <div className="p-3 flex-1 text-left text-sm tracking-wider text-center">Title</div>
                             <div className="p-3 w-1/6 text-left text-sm tracking-wider text-center">Previous Price</div>
                             <div className="p-3 w-1/6 text-left text-sm tracking-wider text-center">Price</div>
-                            {/* Additional Headers */}
                             <div className="p-3 w-1/6 text-left text-sm tracking-wider text-center">Category</div>
                             <div className="p-3 w-1/6 text-left text-sm tracking-wider text-center">Availability</div>
                             <div className="p-3 w-1/6 text-left text-sm tracking-wider text-center">Actions</div>
@@ -38,49 +43,48 @@ const Compare = () => {
                     </div>
                     <div className="bg-gray-800 text-white">
                         {compareData.map((product, index) => (
-                            <div key={index} className="flex hover:bg-opacity-50 bg-black bg-opacity-20">
-                                <div className="p-3  flex items-center justify-start gap-2 flex-1 whitespace-nowrap">
-                                    <img src={product.image1!} className="w-8 h-8 rounded-md" alt={product.title} />
-
-                                    <span className="text-sm font-medium">{product.title}</span>
-                                </div>
-                                <div className="p-3 text-sm w-1/6 whitespace-nowrap  flex items-center justify-center">
-                                    {
-                                        product.previousPrice > 0 ?  <FormattedPrice className=" text-sm" amount={product.previousPrice} />: "Not Provided"
-                                    }
-                                   
-                                    
+                            <React.Fragment key={index}>
+                                <div className="flex hover:bg-opacity-50 bg-black bg-opacity-20" onClick={() => toggleDescription(product.id)}>
+                                    <div className="p-3  flex items-center justify-start gap-2 flex-1 whitespace-nowrap">
+                                        <img src={product.image1!} className="w-8 h-8 rounded-md" alt={product.title} />
+                                        <span className="text-sm font-medium">{product.title}</span>
                                     </div>
-                                <div className="p-3 w-1/6 whitespace-nowrap flex items-center justify-center"><FormattedPrice className=" text-sm" amount={product.price} /></div>
-
-                                {/* Additional Columns */}
-
-                                <div className="p-3 w-1/6 whitespace-nowrap text-sm  flex items-center justify-center">{product.category}</div>
-                                <div className="p-3 w-1/6 whitespace-nowrap text-sm flex items-center justify-center">{product?.count > 0 ? <Check className="text-green-500" /> : <X className="text-rose-500" />}</div>
-                                <div className="p-3 w-1/6 whitespace-nowrap text-sm gap-1 flex items-center justify-center">
-                                    <Link href={`/id_${product.id}`} className="hover:text-designColor">
-                                        <Link2Icon size={19} />
-
-                                    </Link>
-                                    <Heart
-
-                                        onClick={() => {
-                                            dispatch(addToFavorite(product));
-                                            if (isFavorite(product?.id)) {
-                                                toast.error(`${product?.title} removed from favorites!`);
-                                            } else {
-                                                toast.success(`${product?.title} added to favorites!`);
-                                            }
-                                        }}
-                                        size={19}
-                                        className={`block w-5 xs:w-fit cursor-pointer duration-200 hover:text-rose-400 ${isFavorite(product.id) ? 'text-rose-400':"text-zinc-500 "}`}
-                                    />
-                                    <Trash className="text-rose-400 cursor-pointer ml-4" size={18} onClick={() => {
-                                        dispatch(deleteCompare(product));
-                                        toast.success(`${product?.title} is removed from Compare Table!`);
-                                    }} />
+                                    <div className="p-3 text-sm w-1/6 whitespace-nowrap  flex items-center justify-center">
+                                        {product.previousPrice > 0 ? <FormattedPrice className=" text-sm" amount={product.previousPrice} /> : "Not Provided"}
+                                    </div>
+                                    <div className="p-3 w-1/6 whitespace-nowrap flex items-center justify-center">
+                                        <FormattedPrice className=" text-sm" amount={product.price} />
+                                    </div>
+                                    <div className="p-3 w-1/6 whitespace-nowrap text-sm  flex items-center justify-center">{product.category}</div>
+                                    <div className="p-3 w-1/6 whitespace-nowrap text-sm flex items-center justify-center">{product?.count > 0 ? <Check className="text-green-500" /> : <X className="text-rose-500" />}</div>
+                                    <div className="p-3 w-1/6 whitespace-nowrap text-sm gap-1 flex items-center justify-center">
+                                        <Link href={`/id_${product.id}`} className="hover:text-designColor">
+                                            <Link2Icon size={19} />
+                                        </Link>
+                                        <Heart
+                                            onClick={() => {
+                                                dispatch(addToFavorite(product));
+                                                if (isFavorite(product?.id)) {
+                                                    toast.error(`${product?.title} removed from favorites!`);
+                                                } else {
+                                                    toast.success(`${product?.title} added to favorites!`);
+                                                }
+                                            }}
+                                            size={19}
+                                            className={`block w-5 xs:w-fit cursor-pointer duration-200 hover:text-rose-400 ${isFavorite(product.id) ? 'text-rose-400' : "text-zinc-500 "}`}
+                                        />
+                                        <Trash className="text-rose-400 cursor-pointer ml-4" size={18} onClick={() => {
+                                            dispatch(deleteCompare(product));
+                                            toast.success(`${product?.title} is removed from Compare Table!`);
+                                        }} />
+                                    </div>
                                 </div>
-                            </div>
+                                {expandedProduct === product.id && (
+                                    <div className="bg-gray-600 p-3">
+                                        <p className="text-sm px-5">{product.description}</p>
+                                    </div>
+                                )}
+                            </React.Fragment>
                         ))}
                     </div>
                 </div>
