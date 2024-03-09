c6613c1cbb4c4367e0277738cb1fe0a38d77287e
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-03-09 16:25:24 +0000

    Update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,7 +7,7 @@ const page = async () => {
   return (
     <Container className="py-10">
       <div className="border-b w-full border-gray-400 pb-4 flex items-center justify-between">
-        <div className="space-x-4 sm:w-[90%] mx-auto">
+        <div className="space-x-4 sm:w-[98%] mx-auto">
           {/* Lucide Printer icon */}
           <div className="flex items-center space-x-4">
             {/* Lucide Printer icon */}
