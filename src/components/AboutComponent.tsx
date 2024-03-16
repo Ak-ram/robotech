596a8738510a5a324d880d617b7ea331a2ef173b
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-03-16 13:05:10 +0000

    UPDate

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { FileQuestion, Info, ListTodo } from 'lucide-react';
+import { FileQuestion, Info, Link2, ListTodo } from 'lucide-react';
 import Link from 'next/link';
 import { getAboutData } from '@/helpers/getAboutData';
 
@@ -48,15 +48,18 @@ function AboutComponent() {
                 <div className="grid gap-10 md:grid-cols-2">
                     {data.map((item) => (
                         <div key={item.id} className="border overflow-hidden shadow-lg border-gray-500/50 group bg-white rounded-lg shadow-md">
-                            <img className='w-full h-[250px] filter grayscale shadow-xl group-hover:grayscale-0 transition duration-300 ease-in-out' src={item?.image_url} alt={item?.title} />
+                            <div className='overflow-hidden w-full h-[250px]'>
+                            <img className='w-full h-full hover:scale-110 filter grayscale shadow-xl group-hover:grayscale-0 transition duration-300 ease-in-out' src={item?.image_url} alt={item?.title} />
+
+                            </div>
                             <div className='p-6'>
 
                                 <div>
                                     <h6 className="text-xl font-bold uppercase leading-5 text-gray-800 mb-3">{item.title}</h6>
                                     <p className=" text-gray-700 mb-3">{item.description}</p>
-                                    <Link href={item.link_url} className="inline-block bg-indigo-600 text-white py-2 px-4 rounded-md font-semibold transition-colors duration-200 hover:bg-indigo-700">
+                                    <Link href={item.link_url} className="inline-block flex gap-1 items-center justify-center bg-slate-500 text-white py-2 px-4 rounded-md font-semibold transition-colors duration-200 hover:bg-slate-600">
+<Link2 />
                                         {item.link_text.charAt(0).toUpperCase() + item.link_text.slice(1).toLowerCase()}
-
                                     </Link>
                                 </div>
                             </div>
