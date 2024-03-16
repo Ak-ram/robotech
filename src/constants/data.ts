1e60c1e39e2066f8a5c70da24e3e924623038130
akram-mona <157877903+akram-mona@users.noreply.github.com>
2024-03-16 02:31:46 +0000

    Update

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,7 +1,7 @@
 export const navigation = [
   { _id: 910, title: "shop", href: "/" },
-  { _id: 911, title: "Printing", href: "/3d" },
+  { _id: 911, title: "services", href: "/3d" },
   { _id: 912, title: "courses", href: "/courses" },
-  { _id: 913, title: "about", href: "/about" },
+  { _id: 913, title: "news", href: "/about" },
   { _id: 914, title: "faq", href: "/faq" },
 ];
