5d83112dcc54469e9fe0ef42b87b2c0014aa5b76
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2024-03-18 13:40:49 +0000

    Update

--- src/components/FilterableProducts.tsx
+++ src/components/FilterableProducts.tsx
@@ -1,26 +1,26 @@
-'use client'
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import Categories from './Categories';
-import Product from './Product';
-import { getCategoryProducts } from '@/helpers/getCategoryProducts';
-import { getProducts } from '@/helpers/getProducts';
-import { AlignJustify, LibraryBig, Search, X } from 'lucide-react';
-import FormattedPrice from './FormattedPrice';
-import { ProductType, StateProps } from '../../type';
-import { useSelector } from 'react-redux';
-import Link from 'next/link';
-import SearchComponent from './SearchComponent';
+"use client";
+import { createClient } from "@supabase/supabase-js";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import Categories from "./Categories";
+import Product from "./Product";
+import { getCategoryProducts } from "@/helpers/getCategoryProducts";
+import { getProducts } from "@/helpers/getProducts";
+import { AlignJustify, LibraryBig, Search, X } from "lucide-react";
+import FormattedPrice from "./FormattedPrice";
+import { ProductType, StateProps } from "../../type";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+import SearchComponent from "./SearchComponent";
 // import { AlignJustify } from 'lucide-react';
 
 function FilterableProducts({ categories }) {
-  const [categoryName, setCategoryName] = useState('');
-  const [products, setProducts] = useState<any[]>([]);
+  const [categoryName, setCategoryName] = useState("");
+  const [products, setProducts] = useState<any>([]);
   const [totalAmt, setTotalAmt] = useState(0);
   const [rowPrice, setRowPrice] = useState(0);
   const [openSidebar, setOpenSidebar] = useState(false);
 
   // Function to toggle the slide-bottom class on click
-
 
   const { productData, favoriteData } = useSelector(
     (state: StateProps) => state.pro
@@ -39,21 +39,27 @@ function FilterableProducts({ categories }) {
     setRowPrice(rowAmt);
   }, [productData]);
 
-
-
-
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const p = categoryName ? await getCategoryProducts(categoryName) : await getProducts();
-        setProducts(p);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
+        const supabase = createClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL!,
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+          { db: { schema: "products" } }
+        );
 
+        // Make a request
+        const { data, error } = categoryName
+          ? await supabase.from(categoryName.toLowerCase()).select("*")
+          : await supabase.from("sensors").select("*");
+        console.log(categoryName);
+        setProducts(data!);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
     };
 
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       // Run the effect only in the browser environment
       fetchProducts();
     }
@@ -76,17 +82,31 @@ function FilterableProducts({ categories }) {
   //   }
   // }, []);
 
-
   return (
-    <div className='mt-3 md:mt-0'>
-      <div className={`relative flex ${openSidebar ? "gap-2" : "gap-0"} overflow-auto m-auto `}>
-        <Categories products={products} setOpenSidebar={setOpenSidebar} openSidebar={openSidebar} setCategoryName={setCategoryName} categories={categories} />
-        <div className='flex-1 bg-white pt-5 md:rounded-xl'>
-          {openSidebar ? null :
-            <AlignJustify className='cursor-pointer bg-white p-1 rounded w-8 h-8 text-blue-500  ml-3 mr-auto' onClick={() => setOpenSidebar(true)} />
-          }
+    <div className="mt-3 md:mt-0">
+      <div
+        className={`relative flex ${
+          openSidebar ? "gap-2" : "gap-0"
+        } overflow-auto m-auto `}
+      >
+        <Categories
+          products={products}
+          setOpenSidebar={setOpenSidebar}
+          openSidebar={openSidebar}
+          setCategoryName={setCategoryName}
+          categories={categories}
+        />
+        <div className="flex-1 bg-white pt-5 md:rounded-xl">
+          {openSidebar ? null : (
+            <AlignJustify
+              className="cursor-pointer bg-white p-1 rounded w-8 h-8 text-blue-500  ml-3 mr-auto"
+              onClick={() => setOpenSidebar(true)}
+            />
+          )}
           <div className="hidden lg:flex flex-col gap-2 items-center">
-            <h2 className="text-2xl text-designColor font-bold lg:text-3xl ">Choose a Category</h2>
+            <h2 className="text-2xl text-designColor font-bold lg:text-3xl ">
+              Choose a Category
+            </h2>
             <p className="text-sm lg:text-lg text-slate-400 text-center">
               Explore custom layouts designed for seamless electronic shopping.
             </p>
@@ -95,9 +115,16 @@ function FilterableProducts({ categories }) {
 
             <SearchComponent />
           </div> */}
-          <div className={`${openSidebar ? "blur-md hidden md:flex" : "blur-none"} `}>
-
-            <Product categoryName={categoryName} prefix={'pr'} products={products} />
+          <div
+            className={`${
+              openSidebar ? "blur-md hidden md:flex" : "blur-none"
+            } `}
+          >
+            <Product
+              categoryName={categoryName}
+              prefix={"pr"}
+              products={products}
+            />
           </div>
         </div>
       </div>
