c8b1fbff79ce19e4982f28d57feb380d55269217
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2024-03-22 19:39:05 +0000

    Update

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,114 +1,184 @@
-import { addUser } from '@/redux/proSlice';
-import { Key } from 'lucide-react';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
+import { addUser } from "@/redux/proSlice";
+import { Key } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import supabase from "@/supabase/config";
+import toast, { Toast } from "react-hot-toast";
 const LoginComponent = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState(''); // Combined message state
+  // const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(""); // Combined message state
   const [isAuth, setIsAuth] = useState(false); // Combined message state
-  const [route, setRoute] = useState('');
+  const [route, setRoute] = useState("");
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
   const dispatch = useDispatch();
   const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setIsSubmitDisabled(!e.target.value || !password);
-    setMessage(''); // Clear previous messages when input changes
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    setIsSubmitDisabled(!email || !e.target.value);
-    setMessage(''); // Clear previous messages when input changes
+    const enteredEmail = e.target.value;
+    setEmail(enteredEmail);
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+    // Test if the entered email matches the regex pattern
+    const isValidEmail = emailRegex.test(enteredEmail);
+    
+    if (!isValidEmail) {
+      setMessage("Please enter a valid email address.");
+      setIsSubmitDisabled(true);
+    }else{
+      
+      setIsSubmitDisabled(false);
+      setMessage(""); // Clear previous messages when input changes
+    
+    }
   };
   
-    const handleSubmit = () => {
-      // if (email === 'ibrahem' && password === process.env.NEXT_PUBLIC_AUTH_TOKEN) {
-        if (email === process.env.NEXT_PUBLIC_AUTH_USERNAME && password === process.env.NEXT_PUBLIC_AUTH_PASSWORD) {
-        // Assuming you want to dispatch user information when the login is successful
-        const userInformation = { email, password };
-        dispatch(addUser(userInformation));
-    
-        setMessage('You are authorized to login.'); // Set authorized message
-        setIsAuth(true);
-        setRoute('/admin');
-      } else {
-        setMessage('Forbidden: Incorrect username or password.'); // Set error message
-        setIsAuth(false);
+
+  
+  // const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUsername(e.target.value);
+  //   setIsSubmitDisabled(!e.target.value || !email);
+  //   setMessage(""); // Clear previous messages when input changes
+  // };
+
+  const handleSubmit = async () => {
+    // if (email === 'ibrahem' && password === process.env.NEXT_PUBLIC_AUTH_TOKEN) {
+    if (
+      email === 'dd' 
+      // && username === process.env.NEXT_PUBLIC_AUTH_PASSWORD
+    ) {
+      // Assuming you want to dispatch user information when the login is successful
+      // const userInformation = { email, username };
+      const userInformation = { email };
+      dispatch(addUser(userInformation));
+
+      setMessage("You are authorized to login."); // Set authorized message
+      setIsAuth(true);
+      setRoute("/admin");
+      return;
+    } else {
+      // First, perform a query to check if the username or email already exist
+      const { data, error } = await supabase
+        .from("ask_to_be_an_admin")
+        .select("email")
+        .eq("email", email)
+
+      if (error) {
+        console.error("Error checking for existing records:", error.message);
+        toast.error("An error occurred. Please try again later.");
+        return; // Exit the function early if an error occurs
       }
-    };
+
+      // If there are existing records with the same username or email, inform the user and return
+      if (data && data.length > 0) {
+        setMessage(
+          "You have already sent a request. Please wait for the super admin to accept it."
+        );
+        return;
+      }
+
+      // If no existing records are found, proceed with inserting the new record
+      try {
+        await supabase
+          .from("ask_to_be_an_admin")
+          .insert({ email: email });
+        setMessage("Your request was sent to the super admin.");
+        toast.success("Sent 🎉");
+        // setUsername("");
+        setEmail("");
+        setIsAuth(false);
+      } catch (error) {
+        console.error("Error inserting data:", (error as any).message);
+        toast.error(
+          "An error occurred while sending your request. Please try again later."
+        );
+        setMessage(
+          "An error occurred while sending your request. Please try again later."
+        );
+      }
+    }
+  };
 
   return (
-      <div className="relative py-3 w-full">
-        <div className="absolute w-full inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
-        <div className="relative w-full px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div>
-              <h1 className="text-2xl font-semibold">Login as an Admin</h1>
-            </div>
-            <div className="divide-y divide-gray-200">
-              <div className={`${isAuth ? 'hidden' : 'block'} py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7`}>
-                <div className="relative">
-                  <input
-                    id="email"
-                    name="email"
-                    type="text"
-                    value={email}
-                    onChange={handleEmailChange}
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="Email address"
-                  />
-                  <label
-                    htmlFor="email"
-                    className={`absolute left-0 -top-3.5 text-gray-600 text-sm ${email ? '-top-3.5' : 'peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2'} transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
-                  >
-                    Email Address
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="Password"
-                  />
-                  <label
-                    htmlFor="password"
-                    className={`absolute left-0 -top-3.5 text-gray-600 text-sm ${password ? '-top-3.5' : 'peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2'} transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
-                  >
-                    Password
-                  </label>
-                </div>
-                <button
-                  onClick={handleSubmit}
-                  className={`bg-blue-500 '} text-white rounded-md px-2 py-1 ${isSubmitDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-                  disabled={isSubmitDisabled}
-                >
-                  Submit
-                </button>
-              </div>
+    <div className="relative py-3 w-full">
+      <div className="absolute w-full inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+      <div className="relative w-full px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
+        <div className="max-w-md mx-auto">
+          <div>
+            <h1 className="text-2xl font-semibold">Ask to be an Admin</h1>
+          </div>
+          <div className="divide-y divide-gray-200">
+            <div
+              className={`${
+                isAuth ? "hidden" : "block"
+              } py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7`}
+            >
               
-              <div className="relative mt-5">
-                {!isAuth && message && <p className={`${message.includes('authorized') ? 'text-green-500' : 'text-red-500'}`}>{message}</p>}
-                {isAuth && (
-                  <div className='text-sm mt-2'>
-                    <span className='text-center w-full inline-block flex items-center justify-center py-10'><Key className='text-blue-600' size={100} /></span>
-                    You are authorized now, visit
-                    <Link className='underline hover:text-blue-600 mx-1' href={route}>Admin page</Link>
-                    to manage your data
-                  </div>
-                )}
+              <div className="relative">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                  placeholder="Email address"
+                />
+                <label
+                  htmlFor="email"
+                  className={`absolute left-0 -top-3.5 text-gray-600 text-sm ${
+                    email
+                      ? "-top-3.5"
+                      : "peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2"
+                  } transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm`}
+                >
+                  Email Address
+                </label>
               </div>
+
+              <button
+                onClick={handleSubmit}
+                className={`bg-blue-500 '} text-white rounded-md px-2 py-1 ${
+                  isSubmitDisabled ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+                disabled={isSubmitDisabled}
+              >
+                Submit
+              </button>
+            </div>
+
+            <div className="relative mt-5">
+              {!isAuth && message && (
+                <p
+                  className={`${
+                    message.includes("authorized")
+                      ? "text-green-500"
+                      : "text-gray-500"
+                  }`}
+                >
+                  {message}
+                </p>
+              )}
+              {isAuth && (
+                <div className="text-sm mt-2">
+                  <span className="text-center w-full inline-block flex items-center justify-center py-10">
+                    <Key className="text-blue-600" size={100} />
+                  </span>
+                  You are authorized now, visit
+                  <Link
+                    className="underline hover:text-blue-600 mx-1"
+                    href={route}
+                  >
+                    Admin page
+                  </Link>
+                  to manage your data
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
-   
+    </div>
   );
 };
 
