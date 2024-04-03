89834571db5a8d2748007f30a3fd33aac36cb157
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2024-04-03 02:14:21 +0000

    Update

--- src/components/CustomerOrdersList.tsx
+++ src/components/CustomerOrdersList.tsx
@@ -1,15 +1,16 @@
 import { getCustomerOrdersList } from "@/helpers/getCustomerOrdersLists";
 import { X } from "lucide-react";
 import { useEffect, useState } from "react";
-import { CourseType, Order, ProductType, transactionsType } from "../../type";
+import { Bill, CourseType, ProductType, transactionsType } from "../../type";
 import FormattedPrice from "./FormattedPrice";
+import { formatDate } from "@/lib/utils";
 
 const CustomerOrdersList = ({
   showOrdersList,
   setShowOrdersList,
   customerId,
 }) => {
-  const [customerBills, setCustomerBills] = useState([]);
+  const [customerBills, setCustomerBills] = useState<Bill[]>([]); // for fetching customer bills();
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -23,8 +24,8 @@ const CustomerOrdersList = ({
     fetchOrders().then((data) => {
       console.log("customer list of bills", data!);
       let list = [];
-      data!.map((item) => {
-        list.push(item.data);
+      data!.map((item: Bill) => {
+        list.push(item);
       });
       setCustomerBills(list);
     });
@@ -44,20 +45,40 @@ const CustomerOrdersList = ({
         <div className="mt-5">
           <ul>
             {customerBills.length > 0 ? (
-              customerBills.map((item: Order, i) => (
-                <li key={`${item.productName}_${i}`} className="border-b py-4">
-                  <div className="flex items-center justify-between">
+              customerBills.map(({ created_at, data, id }: Bill, i) => (
+                <li key={`${id}_${i}`} className="border-b py-4">
+                  <div className="flex items-center justify-between ">
                     <div>
-                      <h4 className="font-bold">{item.productName}</h4>
-                      <p className="text-gray-500">Date: {item.date}</p>
+                      <h4 className="font-bold">Bill Id: #{id}</h4>
+                      <p className="text-gray-500">
+                        Date: {formatDate(created_at)}
+                      </p>
                     </div>
-
-                    <ul className="flex gap-2 items-center bg-gray-200 p-3 rounded text-sm">
-                      <li>Price: {item.piecePrice} EGP</li>
-                      <li>Quantity: {item.quantity}</li>
-                      <li>Discount: {item.discount}</li>
-                      <li>Subtotal: {item.subtotal}</li>
-                    </ul>
+                    <div className="flex    max-h-[200px] overflow-y-auto flex-col w-[90%] gap-2">
+                      {data.map(
+                        (
+                          {
+                            piecePrice,
+                            quantity,
+                            discount,
+                            subtotal,
+                            productName,
+                          },
+                          j,
+                        ) => (
+                          <ul
+                            key={`${id}_${j}_${piecePrice}`}
+                            className="flex ml-auto overflow-x-auto whitespace-nowrap w-[60%] gap-2 items-center bg-gray-200 p-3 rounded text-sm"
+                          >
+                            <li>Title: {productName}</li>
+                            <li>Price: {piecePrice} EGP</li>
+                            <li>Quantity: {quantity}</li>
+                            <li>Discount: {discount}</li>
+                            <li>Subtotal: {subtotal}</li>
+                          </ul>
+                        ),
+                      )}
+                    </div>
                   </div>
                 </li>
               ))
