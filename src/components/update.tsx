2a6c9468698fa2f28e9bded79bd630fed2823226
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2024-04-03 00:39:20 +0000

    Update

--- src/components/update.tsx
+++ src/components/update.tsx
@@ -1,10 +1,19 @@
 import supabase from "@/supabase/config";
 import { useEffect, useState } from "react";
 import { Bill } from "../../type";
+import { formatDate } from "@/lib/utils";
 
 const TransactionAnalyzer = () => {
   const [bills, setBills] = useState<Bill[]>([]);
-  const [totalSubtotal, setTotalSubtotal] = useState<number>(0);
+  const [dailyStats, setDailyStats] = useState<
+    { date: string; totalSells: number; totalProfit: number }[]
+  >([]);
+  const [monthlyStats, setMonthlyStats] = useState<
+    { month: string; totalSells: number; totalProfit: number }[]
+  >([]);
+  const [yearlyStats, setYearlyStats] = useState<
+    { year: string; totalSells: number; totalProfit: number }[]
+  >([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,45 +29,157 @@ const TransactionAnalyzer = () => {
   }, []);
 
   useEffect(() => {
-    // Calculate total subtotal when bills state changes
-    const calculateTotalSubtotal = () => {
-      const total = bills.reduce((accumulator, bill) => {
-        return (
-          accumulator +
-          bill.data.reduce((subtotalAccumulator, transaction) => {
-            return subtotalAccumulator + transaction.subtotal;
-          }, 0)
+    // Calculate daily stats when bills state changes
+    const calculateDailyStats = () => {
+      const dailyStatsMap = new Map<
+        string,
+        { totalSells: number; totalProfit: number }
+      >();
+
+      bills.forEach((bill) => {
+        const date = formatDate(bill.created_at);
+
+        if (!dailyStatsMap.has(date)) {
+          dailyStatsMap.set(date, { totalSells: 0, totalProfit: 0 });
+        }
+
+        const dailyStat = dailyStatsMap.get(date)!;
+        dailyStat.totalSells += bill.data.reduce(
+          (acc, curr) => acc + curr.subtotal,
+          0,
         );
-      }, 0);
-      setTotalSubtotal(total);
+        dailyStat.totalProfit += bill.data.reduce(
+          (acc, curr) =>
+            acc + (curr.subtotal - curr.wholesalePrice * curr.quantity),
+          0,
+        );
+      });
+
+      const dailyStatsArray = Array.from(dailyStatsMap).map(
+        ([date, { totalSells, totalProfit }]) => ({
+          date,
+          totalSells,
+          totalProfit,
+        }),
+      );
+
+      setDailyStats(dailyStatsArray);
     };
 
-    calculateTotalSubtotal();
+    const calculateMonthlyAndYearlyStats = () => {
+      const monthlyStatsMap = new Map<
+        string,
+        { totalSells: number; totalProfit: number }
+      >();
+      const yearlyStatsMap = new Map<
+        string,
+        { totalSells: number; totalProfit: number }
+      >();
+
+      bills.forEach((bill) => {
+        const date = new Date(bill.created_at);
+        const month = `${date.getMonth() + 1}-${date.getFullYear()}`;
+        const year = `${date.getFullYear()}`;
+
+        if (!monthlyStatsMap.has(month)) {
+          monthlyStatsMap.set(month, { totalSells: 0, totalProfit: 0 });
+        }
+        if (!yearlyStatsMap.has(year)) {
+          yearlyStatsMap.set(year, { totalSells: 0, totalProfit: 0 });
+        }
+
+        const monthlyStat = monthlyStatsMap.get(month)!;
+        const yearlyStat = yearlyStatsMap.get(year)!;
+
+        monthlyStat.totalSells += bill.data.reduce(
+          (acc, curr) => acc + curr.subtotal,
+          0,
+        );
+        monthlyStat.totalProfit += bill.data.reduce(
+          (acc, curr) =>
+            acc + (curr.subtotal - curr.wholesalePrice * curr.quantity),
+          0,
+        );
+
+        yearlyStat.totalSells += bill.data.reduce(
+          (acc, curr) => acc + curr.subtotal,
+          0,
+        );
+        yearlyStat.totalProfit += bill.data.reduce(
+          (acc, curr) =>
+            acc + (curr.subtotal - curr.wholesalePrice * curr.quantity),
+          0,
+        );
+      });
+
+      const monthlyStatsArray = Array.from(monthlyStatsMap).map(
+        ([month, { totalSells, totalProfit }]) => ({
+          month,
+          totalSells,
+          totalProfit,
+        }),
+      );
+      const yearlyStatsArray = Array.from(yearlyStatsMap).map(
+        ([year, { totalSells, totalProfit }]) => ({
+          year,
+          totalSells,
+          totalProfit,
+        }),
+      );
+
+      setMonthlyStats(monthlyStatsArray);
+      setYearlyStats(yearlyStatsArray);
+    };
+
+    calculateDailyStats();
+    calculateMonthlyAndYearlyStats();
   }, [bills]);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Bills Analyzer</h1>
       <div className="grid gap-4">
-        {bills.map((bill) => (
-          <div key={bill.id} className="bg-gray-100 p-4 rounded-lg">
-            <h2 className="text-xl font-semibold mb-2">
-              Customer Name: {bill.customerData.fullName}
-            </h2>
-            <p className="mb-2">Customer ID: {bill.customerData.id}</p>
-            <p className="mb-2">Bill ID: {bill.id}</p>
-            <p className="mb-2">Date: {bill.created_at}</p>
-            <p>
-              Total Subtotal:{" "}
-              {bill.data.reduce((acc, curr) => acc + curr.subtotal, 0)}
-            </p>
-          </div>
-        ))}
-      </div>
-      <div className="mt-4">
-        <h2 className="text-xl font-semibold">
-          Total Subtotal: {totalSubtotal}
-        </h2>
+        <div>
+          <h2 className="text-xl font-semibold mb-2">Daily Profits</h2>
+          {dailyStats.map((dailyStat, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 p-4 rounded-lg mb-2 flex justify-between items-center"
+            >
+              <h3 className="text-lg font-semibold">Date: {dailyStat.date}</h3>
+              <p>Total Sells: {dailyStat.totalSells} L.E</p>
+              <p>Total Profit: {dailyStat.totalProfit} L.E</p>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-2">Monthly Profits</h2>
+          {monthlyStats.map((monthlyStat, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 p-4 rounded-lg mb-2 flex justify-between items-center"
+            >
+              <h3 className="text-lg font-semibold">
+                Month: {monthlyStat.month}
+              </h3>
+              <p>Total Sells: {monthlyStat.totalSells} L.E</p>
+              <p>Total Profit: {monthlyStat.totalProfit} L.E</p>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold mb-2">Yearly Profits</h2>
+          {yearlyStats.map((yearlyStat, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 p-4 rounded-lg mb-2 flex justify-between items-center"
+            >
+              <h3 className="text-lg font-semibold">Year: {yearlyStat.year}</h3>
+              <p>Total Sells: {yearlyStat.totalSells} L.E</p>
+              <p>Total Profit: {yearlyStat.totalProfit} L.E</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,65 +1,114 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Bill } from "../../type";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-
 export function formatFullName(fullName: string) {
-  const words = fullName.split(' ');
+  const words = fullName.split(" ");
 
   if (words.length < 4) {
-    return words.map(name => name.charAt(0).toUpperCase() + name.slice(1)).join(' '); // Return the original full name if it has less than three parts
+    return words
+      .map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+      .join(" "); // Return the original full name if it has less than three parts
   }
 
   const firstName = words[0];
-  const modifiedNames = words.slice(2).map(name => name.charAt(0).toUpperCase() + '.');
+  const modifiedNames = words
+    .slice(2)
+    .map((name) => name.charAt(0).toUpperCase() + ".");
 
-  const formattedName = [firstName, words[1], ...modifiedNames].join(' ');
+  const formattedName = [firstName, words[1], ...modifiedNames].join(" ");
 
   return formattedName;
 }
 
 export function isColor(text: string) {
-  const colorNames = ['black', 'orange', 'lime', 'cyan', 'indigo', 'violet', 'purple', 'rose', 'yellow', 'pink', 'sky', 'red', 'blue', 'green']; // Add more color names as needed
+  const colorNames = [
+    "black",
+    "orange",
+    "lime",
+    "cyan",
+    "indigo",
+    "violet",
+    "purple",
+    "rose",
+    "yellow",
+    "pink",
+    "sky",
+    "red",
+    "blue",
+    "green",
+  ]; // Add more color names as needed
   return colorNames.includes(text.toLowerCase().trim());
-};
-
+}
 
 export function detectLanguage(text) {
   if (!text) return "Unknown";
 
   // Check if the text contains Arabic characters
   if (/[\u0600-\u06FF]/.test(text)) {
-      return "ar";
+    return "ar";
   }
   // Check if the text contains Latin characters
   else if (/^[a-zA-Z]*$/.test(text)) {
-      return "en";
+    return "en";
   }
   // Default to unknown language
   else {
-      return "Unknown";
+    return "Unknown";
   }
 }
 
+export function searchItems(items, searchTerm, searchType) {
+  return items.filter((item) => {
+    // Convert searchTerm to lowercase for case-insensitive search
+    const term = searchTerm.toLowerCase();
 
-
-export function searchItems(items,searchTerm, searchType) {
-  return items.filter(item => {
-      // Convert searchTerm to lowercase for case-insensitive search
-      const term = searchTerm.toLowerCase();
-      
-      // Determine the search behavior based on searchType
-      switch (searchType) {
-          case 'name':
-              return item?.customerData?.fullName?.toLowerCase().includes(term);
-          case 'phone':
-              return item?.customerData?.phone?.toLowerCase().includes(term);
-          case 'id':
-              return item?.id === +term;
-          default:
-              return [];
-      }
+    // Determine the search behavior based on searchType
+    switch (searchType) {
+      case "name":
+        return item?.customerData?.fullName?.toLowerCase().includes(term);
+      case "phone":
+        return item?.customerData?.phone?.toLowerCase().includes(term);
+      case "id":
+        return item?.id === +term;
+      default:
+        return [];
+    }
   });
 }
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-GB");
+};
+
+// billUtils.ts
+
+export const calculateTotalSells = (bills: Bill[]) => {
+  return bills.reduce((accumulator, bill) => {
+    return (
+      accumulator +
+      bill.data.reduce((subtotalAccumulator, transaction) => {
+        return subtotalAccumulator + transaction.subtotal;
+      }, 0)
+    );
+  }, 0);
+};
+
+export const calculateTotalProfit = (bills: Bill[]) => {
+  return bills.reduce((accumulator, bill) => {
+    return (
+      accumulator +
+      bill.data.reduce((profitAccumulator, transaction) => {
+        return (
+          profitAccumulator +
+          (transaction.subtotal -
+            transaction.wholesalePrice * transaction.quantity)
+        );
+      }, 0)
+    );
+  }, 0);
+};
