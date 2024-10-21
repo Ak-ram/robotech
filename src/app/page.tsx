2b7cfa7deda7c6227609e0ca1903f70066b4ca1a
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2024-10-21 16:12:10 +0000

    add animation when user order new product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Banner from "@/components/Banner";
 import Products from "@/components/Products";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
@@ -8,6 +9,16 @@ export default function Home() {
 
       <Banner />
       <Products />
+        
+      <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                background: "#000",
+                color: "#fff",
+              },
+            }}
+          />
     </main>
   );
 }
